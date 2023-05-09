5db65aa16a198a033531c6a0d8639394c417ab79
akash-endlos <akash.endlos@gmail.com>
2023-05-09 10:33:05 -0700

    updated

--- src/components/home-page-components/HomeImageContainer/HomeImageContainer.jsx
+++ src/components/home-page-components/HomeImageContainer/HomeImageContainer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import bg from "../../../../public/assets/home-7-img-9.jpg";
 const HomeImageContainer = () => {
   return (
-    <div className="bg-[#111111] pt-28 ">
+    <div className="bg-[#111111] py-52 h-auto ">
       <div
         style={{
           backgroundImage: `url(${bg.src})`,
@@ -13,25 +13,52 @@ const HomeImageContainer = () => {
           height: "100vh",
         }}
       >
-
-        <div className="flex items-start gap-50">
-          <div className=" transparent-text ">
-            HIM
-          </div>
-          <div className="relative">
-            <img src="/assets/home-7-img-1.jpg" />
-            <div className="absolute bottom-[-59%] left-[-8%]">
-            <h1 className=" text-white  text-7xl font-bold inline-block align-middle">HOW FAR WILL YOU GO?</h1>
-            <p className="text-white pt-5 w-3/4 leading-8">
-            Lorem Ipsn gravida nibh vel velit auctor aliquet. Aene sollic conseut ipsutis sem nibh id elit. Duis sed nibh vel a siteiu amet nibh vulputate. Dolor orem Ipsn vel velitui auctor aliquet re ipsum dolor sit amet rem Ipsn gravida nibh vel velit auctor aliquet. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentu aene.
-            </p>
-            <button className="px-16 uppercase py-3 border-2 border-white text-white my-10 font-semibold tracking-widest">Official Trailer</button>
+        <div class="relative">
+          <img class="ml-auto" src="/assets/home-7-img-1.jpg" alt="Image" />
+          <div class="absolute top-[72%] left-[27%] w-full h-full flex items-center justify-center">
+            <div>
+              <h1 className=" text-white  text-7xl font-bold inline-block align-middle">
+                HOW FAR WILL YOU GO?
+              </h1>
+              <p className="text-white pt-5 w-2/3 leading-8">
+                Lorem Ipsn gravida nibh vel velit auctor aliquet. Aene sollic
+                conseut ipsutis sem nibh id elit. Duis sed nibh vel a siteiu
+                amet nibh vulputate. Dolor orem Ipsn vel velitui auctor aliquet
+                re ipsum dolor sit amet rem Ipsn gravida nibh vel velit auctor
+                aliquet. Curabitur ullamcorper ultricies nisi. Nam eget dui.
+                Etiam rhoncus. Maecenas tempus, tellus eget condimentu aene.
+              </p>
+              <button className="px-16 uppercase py-3 border-2 border-white text-white my-10 font-semibold tracking-widest">
+                Official Trailer
+              </button>
             </div>
           </div>
         </div>
       </div>
-     </div>
+    </div>
   );
 };
 
 export default HomeImageContainer;
+
+<div className="flex justify-center items-center gap-50">
+  <div className="relative">
+    <img src="/assets/home-7-img-1.jpg" />
+    <div className="absolute bottom-[-59%] left-[-8%]">
+      <h1 className=" text-white  text-7xl font-bold inline-block align-middle">
+        HOW FAR WILL YOU GO?
+      </h1>
+      <p className="text-white pt-5 w-3/4 leading-8">
+        Lorem Ipsn gravida nibh vel velit auctor aliquet. Aene sollic conseut
+        ipsutis sem nibh id elit. Duis sed nibh vel a siteiu amet nibh
+        vulputate. Dolor orem Ipsn vel velitui auctor aliquet re ipsum dolor sit
+        amet rem Ipsn gravida nibh vel velit auctor aliquet. Curabitur
+        ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas
+        tempus, tellus eget condimentu aene.
+      </p>
+      <button className="px-16 uppercase py-3 border-2 border-white text-white my-10 font-semibold tracking-widest">
+        Official Trailer
+      </button>
+    </div>
+  </div>
+</div>;
--- src/components/home-page-components/HomeVideoCarousel/HomeVideoCarousel.jsx
+++ src/components/home-page-components/HomeVideoCarousel/HomeVideoCarousel.jsx
@@ -1,4 +1,3 @@
-
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -6,45 +5,56 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/swiper.min.css";
 
-
-
 // import Swiper core and required modules
-import SwiperCore, {
-  EffectCoverflow, Pagination
-} from 'swiper/core';
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper/core";
 import { Navigation } from "swiper";
 
 // install Swiper modules
 SwiperCore.use([EffectCoverflow, Pagination, Navigation]);
 
-
 export default function HomeVideoCarousel() {
-
-
-
   return (
-    <>
-      <Swiper loop={true} effect={'coverflow'} grabCursor={true} centeredSlides={true} slidesPerView={1.5} coverflowEffect={{
-        "rotate": 0,
-        "stretch": 0,
-        "depth": 200,
-        "modifier": 1,
-        "scale": 2,
-        "slideShadows": false
-      }} pagination={true} navigation={true} className="mySwiper">
-       <SwiperSlide >
-            <video src="/assets/cr2.mp4" />
-          </SwiperSlide>
-          <SwiperSlide >
-            <video src="/assets/cr3.mp4" />
-          </SwiperSlide>
-          <SwiperSlide >
-            <video src="/assets/cr4.mp4" />
-          </SwiperSlide>
-          <SwiperSlide >
-            <video src="/assets/cr5.mp4" />
-          </SwiperSlide>
+    <div className="pt-20 bg-[#111111]">
+      <Swiper
+        loop={true}
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={2}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 200,
+          modifier: 1,
+          scale: 2,
+          slideShadows: false,
+        }}
+        pagination={true}
+        navigation={true}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <video src="/assets/cr2.mp4" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <video src="/assets/cr3.mp4" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <video src="/assets/cr4.mp4" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <video src="/assets/cr5.mp4" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <video src="/assets/cr5.mp4" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <video src="/assets/cr5.mp4" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <video src="/assets/cr5.mp4" />
+        </SwiperSlide>
       </Swiper>
-    </>
-  )
+    </div>
+  );
 }

0edf51cb091eb1e1c9e0cfca5dffb83746d69511
akash-endlos <akash.endlos@gmail.com>
2023-05-10 14:38:09 +0530

    updated

---
+++ src/pages/company/faq/index.js
@@ -0,0 +1,17 @@
+import Footer from '@/components/footer/Footer'
+import Navbar from '@/components/navbar/Navbar'
+import React from 'react'
+
+const index = () => {
+  return (
+    <>
+    <Navbar Navclass="bg-gray-500 text-white"/>
+     <div className="pt-24">
+         FAQ
+     </div>
+      <Footer />
+    </>
+  )
+}
+
+export default index

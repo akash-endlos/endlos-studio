56bf293290c507ca288656a3522b6d89b5980359
akash-endlos <akash.endlos@gmail.com>
2023-05-11 15:36:34 +0530

    updated

--- src/components/portfolio-page-components/PortFolioParallax/PortFolioParallax.jsx
+++ src/components/portfolio-page-components/PortFolioParallax/PortFolioParallax.jsx
@@ -1,26 +1,36 @@
 import React from 'react'
 import bg from "../../../../public/assets/img1.jpg";
 
-const PortFolioParallax = () => {
-  return (
-    <div class="relative">
-    <div class="h-screen bg-cover bg-fixed" style={{
-        backgroundImage: `url(${bg.src})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        width: "100vw",
-        height: "300px",
-    }}>
-        <div class="absolute inset-0 bg-black opacity-60"></div>
-        <div class="absolute inset-0 flex justify-center items-center">
-            <div class="text-center">
-                <h1 class="text-5xl font-bold text-white">Archwiz</h1>
-                <p class="mt-4 text-lg text-white font-serif">Can You Handle The Truth</p>
+const PortFolioParallax = ({ title, subtitle }) => {
+    return (
+        <>
+            <div class="relative">
+                <div class="h-screen bg-cover bg-fixed" style={{
+                    backgroundImage: `url(${bg.src})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    width: "100vw",
+                    height: "300px",
+                }}>
+                    <div class="absolute inset-0 bg-black opacity-60"></div>
+                    <div class="absolute inset-0 flex justify-center items-center">
+                        <div class="text-center">
+                            <h1 class="text-5xl font-bold text-white">{title}</h1>
+                            <p class="mt-4 text-lg text-white font-serif">{subtitle}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</div>
-  )
+            <div class="bg-black flex justify-center items-center max-h-max md:h-80 lg:h-96 p-5">
+                <div class="inset-0 flex justify-center items-center">
+                    <div class="text-center">
+                        <h1 class="text-5xl font-bold text-white">{title}</h1>
+                        <p class="mt-4 text-lg text-white font-serif max-w-screen-xl text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem voluptas sunt dolores reprehenderit, quo saepe earum nesciunt magnam officiis incidunt totam dolorem suscipit veniam temporibus adipisci molestias ipsa rem. Odit optio rerum atque neque nulla ipsam quaerat reiciendis hic perferendis, et harum veritatis, architecto quidem eveniet</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default PortFolioParallax
--- src/pages/portfolio/archviz/index.jsx
+++ src/pages/portfolio/archviz/index.jsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 import bg from "../../../../public/assets/img1.jpg";
 import PortFolioParallax from '@/components/portfolio-page-components/PortFolioParallax/PortFolioParallax';
+import ArchwizGallery from '@/components/portfolio-page-components/ArchwizGallery/ArchwizGallery';
 
 
 const index = () => {
@@ -24,65 +25,22 @@ const index = () => {
         <>
             <Navbar Navclass="bg-[#111111] text-white" />
             <div className="pt-24 bg-[#111111] text-white">
-                <PortFolioParallax />
-                <div class="bg-black flex justify-center items-center max-h-max md:h-80 lg:h-96 p-5">
+                <PortFolioParallax title={'Archviz'} subtitle={'Can You Handle The Truth'} />
+                {/* <div class="bg-black flex justify-center items-center max-h-max md:h-80 lg:h-96 p-5">
                     <div class="inset-0 flex justify-center items-center">
                         <div class="text-center">
                             <h1 class="text-5xl font-bold text-white">Archwiz</h1>
                             <p class="mt-4 text-lg text-white font-serif max-w-screen-xl text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem voluptas sunt dolores reprehenderit, quo saepe earum nesciunt magnam officiis incidunt totam dolorem suscipit veniam temporibus adipisci molestias ipsa rem. Odit optio rerum atque neque nulla ipsam quaerat reiciendis hic perferendis, et harum veritatis, architecto quidem eveniet</p>
                         </div>
                     </div>
+                </div> */}
+                <div className='grid grid-cols-2 gap-4'>
+                 <div>
+                                    
+                 </div>
                 </div>
-                
-<div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt=""/>
-        </div>
-    </div>
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt=""/>
-        </div>
-    </div>
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt=""/>
-        </div>
-    </div>
-    <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt=""/>
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt=""/>
-        </div>
-    </div>
-</div>
-
-
-
+                <ArchwizGallery/>
+                <PortFolioParallax title={'WalkThrough'} subtitle={'Can You Handle The Truth'}/>
 
             </div>
             <Footer />

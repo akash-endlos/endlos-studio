76c076920180613cda6726a68ef3cc1d0b709c8c
akash-endlos <akash.endlos@gmail.com>
2023-05-11 17:03:33 +0530

    updated

---
+++ src/components/portfolio-page-components/PortFolioCardsContainer/PortFolioCardsContainer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const PortFolioCardsContainer = () => {
+    return (
+        <div className="mx-auto max-w-full p-5  ">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 pb-10 gap-4">
+                <div className=" py-4 ">
+                    <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
+                    <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">DIRECTOR</h2>
+                    <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div className=" py-4 ">
+                    <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
+                    <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">DIRECTOR</h2>
+                    <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div className=" py-4 ">
+                    <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
+                    <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">DIRECTOR</h2>
+                    <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div className=" py-4 ">
+                    <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
+                    <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">DIRECTOR</h2>
+                    <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PortFolioCardsContainer
--- src/pages/portfolio/virtual-production/index.js
+++ src/pages/portfolio/virtual-production/index.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 import PortFolioParallax from '@/components/portfolio-page-components/PortFolioParallax/PortFolioParallax';
 import PortFolioCarousel from '@/components/portfolio-page-components/PortFolioCarousel/PortFolioCarousel';
+import PortFolioCardsContainer from '@/components/portfolio-page-components/PortFolioCardsContainer/PortFolioCardsContainer';
 
 
 const index = () => {
@@ -21,16 +22,36 @@ const index = () => {
             <Navbar Navclass="bg-[#111111] text-white" />
             <div className="pt-24 bg-[#111111] text-white">
                 <PortFolioParallax title={'Virtual production'} subtitle={'Can You Handle The Truth'} />
-
+                  <div className='max-w-screen-xl mx-auto'>
+                  <div className='py-10'>
+                  <video src='/assets/1.mp4' controls width="100%"></video>
+                  <div className='text-xl py-5 font-serif text-justify'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem voluptas sunt dolores reprehenderit, quo saepe earum nesciunt magnam officiis incidunt totam dolorem suscipit veniam temporibus adipisci molestias ipsa rem. Odit optio rerum atque neque nulla ipsam quaerat reiciendis hic perferendis, et harum veritatis, architecto quidem eveniet
+                  </div>
+                  </div>
+                  </div>
+                  <PortFolioCardsContainer/>
                 <div id="environment">
                     <PortFolioParallax title={'Environment'} subtitle={'Can You Handle The Truth'} btn={true} />
-                    <PortFolioCarousel />
+                    <div className='flex justify-center items-center'>
+                        <video src='/assets/1.mp4' controls width="100%"></video>
+                    </div>
+                  
                 </div>
                 <div id="arvr">
                     <PortFolioParallax title={'AR & VR'} subtitle={'Can You Handle The Truth'} />
                     <div className='flex justify-center items-center'>
                         <video src='/assets/1.mp4' controls width="100%"></video>
                     </div>
+                    <PortFolioCarousel />
+                    <div className='max-w-screen-xl mx-auto'>
+                  <div className='py-10'>
+                  <video src='/assets/1.mp4' controls width="100%"></video>
+                  <div className='text-xl py-5 font-serif text-justify'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem voluptas sunt dolores reprehenderit, quo saepe earum nesciunt magnam officiis incidunt totam dolorem suscipit veniam temporibus adipisci molestias ipsa rem. Odit optio rerum atque neque nulla ipsam quaerat reiciendis hic perferendis, et harum veritatis, architecto quidem eveniet
+                  </div>
+                  </div>
+                  </div>
                 </div>
             </div>
             <Footer />

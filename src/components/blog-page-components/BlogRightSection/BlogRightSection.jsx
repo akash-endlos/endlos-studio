b84c92466c3a08b751d9ff389227f60109538f7e
akash-endlos <akash.endlos@gmail.com>
2023-05-12 12:44:48 +0530

    updated

---
+++ src/components/blog-page-components/BlogRightSection/BlogRightSection.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const BlogRightSection = () => {
+    return (
+        <div className='px-1 py-10'>
+            <h1 className="text-3xl py-5">Popular News</h1>
+            <div className='flex flex-col justify-between items-center text-white gap-4'>
+
+                <a href="#" class="flex flex-col items-center  shadow xl:flex-row md:max-w-xl  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                    <img class="object-cover w-full h-100 md:h-auto xl:w-36" src="/assets/home-7-img-1.jpg" alt="" />
+                    <div class="flex flex-col justify-between px-2 leading-normal">
+                        <h2 class="mb-2  font-bold tracking-tight ">Noteworthy technology</h2>
+                        <p>Read More</p>
+                    </div>
+                </a>
+                <a href="#" class="flex flex-col items-center  shadow xl:flex-row md:max-w-xl  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                    <img class="object-cover w-full h-100 md:h-auto xl:w-36" src="/assets/home-7-img-1.jpg" alt="" />
+                    <div class="flex flex-col justify-between px-2 leading-normal">
+                        <h2 class="mb-2 font-bold tracking-tight ">Noteworthy technology</h2>
+                        <p>Read More</p>
+                    </div>
+                </a>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default BlogRightSection
--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,6 +1,7 @@
 import ParallaxImageContainer from '@/components/about-page-components/ParallaxImageContainer/ParallaxImageContainer'
 import AlternateImageContainer from '@/components/blog-page-components/AlternateImageContainer/AlternateImageContainer'
 import BlogParallaxComponent from '@/components/blog-page-components/BlogParallaxComponent/BlogParallaxComponent'
+import BlogRightSection from '@/components/blog-page-components/BlogRightSection/BlogRightSection'
 import Footer from '@/components/footer/Footer'
 import Navbar from '@/components/navbar/Navbar'
 import React from 'react'
@@ -10,14 +11,14 @@ const index = () => {
         <>
             <Navbar />
             <BlogParallaxComponent />
-            <div class="grid grid-cols-1 xl:grid-cols-5 gap-4 ">
+            <div class="grid grid-cols-1 xl:grid-cols-5 gap-4 bg-[#111111] ">
                 <div class="col-span-4">
                     <AlternateImageContainer />
                     <AlternateImageContainer />
                     <AlternateImageContainer />
                 </div>
                 <div class="col-span-1 text-white">
-
+                <BlogRightSection/>
                 </div>
             </div>
             <Footer />

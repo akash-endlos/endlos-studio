2d85f2c509f758d84cdf1e985f792c7d079a3254
akash-endlos <akash.endlos@gmail.com>
2023-05-12 13:34:16 +0530

    updated

--- src/components/blog-page-components/BlogRightSection/BlogRightSection.jsx
+++ src/components/blog-page-components/BlogRightSection/BlogRightSection.jsx
@@ -2,55 +2,64 @@ import React from 'react'
 
 const BlogRightSection = () => {
     return (
-        <div className='px-1 py-10'>
-            <h1 className="text-3xl py-5">Popular News</h1>
-            <div className='flex flex-col justify-between items-center text-white gap-4'>
-
-                <a href="#" class="flex flex-col items-center  shadow xl:flex-row md:max-w-xl  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                    <img class="object-cover w-full h-100 md:h-auto xl:w-36" src="/assets/home-7-img-1.jpg" alt="" />
-                    <div class="flex flex-col justify-between px-2 leading-normal">
-                        <h2 class="mb-2  font-bold tracking-tight ">Noteworthy technology</h2>
-                        <p>Read More</p>
-                    </div>
-                </a>
-                <a href="#" class="flex flex-col items-center  shadow xl:flex-row md:max-w-xl  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                    <img class="object-cover w-full h-100 md:h-auto xl:w-36" src="/assets/home-7-img-1.jpg" alt="" />
-                    <div class="flex flex-col justify-between px-2 leading-normal">
-                        <h2 class="mb-2 font-bold tracking-tight ">Noteworthy technology</h2>
-                        <p>Read More</p>
-                    </div>
-                </a>
-
-
-            </div>
-            <h1 className="text-3xl py-5">About</h1>
+        <div className='px-1 py-5 w-full'>
+            <h1 className="text-3xl font-bold pb-5">Search</h1>
             <div className=' text-white gap-4 px-5'>
-
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur magnam officiis rerum!</p>
-
-
+                <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required />
             </div>
-            <h1 className="text-3xl py-5">Popular News</h1>
-            <div className='flex flex-col justify-between items-center text-white gap-4'>
+            <div class="flex flex-col xl:items-start items-center py-10">
+                <h1 className="text-3xl font-bold">About</h1>
+                <div className=' text-white gap-4 px-5 pt-5'>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur magnam officiis rerum!</p>
+                </div>
+                <h1 className="text-3xl font-bold py-5">Popular News</h1>
+                <div className='flex flex-col justify-between items-center text-white gap-4'>
 
-                <a href="#" class="flex flex-col items-center  shadow xl:flex-row md:max-w-xl  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                    <img class="object-cover w-full h-100 md:h-auto xl:w-36" src="/assets/home-7-img-1.jpg" alt="" />
-                    <div class="flex flex-col justify-between px-2 leading-normal">
-                        <h2 class="mb-2  font-bold tracking-tight ">Noteworthy technology</h2>
-                        <p>Read More</p>
-                    </div>
-                </a>
-                <a href="#" class="flex flex-col items-center  shadow xl:flex-row md:max-w-xl  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                    <img class="object-cover w-full h-100 md:h-auto xl:w-36" src="/assets/home-7-img-1.jpg" alt="" />
-                    <div class="flex flex-col justify-between px-2 leading-normal">
-                        <h2 class="mb-2 font-bold tracking-tight ">Noteworthy technology</h2>
-                        <p>Read More</p>
-                    </div>
-                </a>
+                    <a class="flex flex-col items-center  shadow xl:flex-row md:max-w-xl  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <img class="object-cover w-full h-100 md:h-auto xl:w-36" src="/assets/home-7-img-1.jpg" alt="" />
+                        <div class="flex flex-col justify-between px-2 leading-normal">
+                            <h2 class="mb-2  font-bold tracking-tight ">Noteworthy technology</h2>
+                            <p>Read More</p>
+                        </div>
+                    </a>
+                    <a class="flex flex-col items-center  shadow xl:flex-row md:max-w-xl  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <img class="object-cover w-full h-100 md:h-auto xl:w-36" src="/assets/home-7-img-1.jpg" alt="" />
+                        <div class="flex flex-col justify-between px-2 leading-normal">
+                            <h2 class="mb-2 font-bold tracking-tight ">Noteworthy technology</h2>
+                            <p>Read More</p>
+                        </div>
+                    </a>
+                    <a class="flex flex-col items-center  shadow xl:flex-row md:max-w-xl  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <img class="object-cover w-full h-100 md:h-auto xl:w-36" src="/assets/home-7-img-1.jpg" alt="" />
+                        <div class="flex flex-col justify-between px-2 leading-normal">
+                            <h2 class="mb-2 font-bold tracking-tight ">Noteworthy technology</h2>
+                            <p>Read More</p>
+                        </div>
+                    </a>
+                    <a class="flex flex-col items-center  shadow xl:flex-row md:max-w-xl  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <img class="object-cover w-full h-100 md:h-auto xl:w-36" src="/assets/home-7-img-1.jpg" alt="" />
+                        <div class="flex flex-col justify-between px-2 leading-normal">
+                            <h2 class="mb-2 font-bold tracking-tight ">Noteworthy technology</h2>
+                            <p>Read More</p>
+                        </div>
+                    </a>
 
 
+                </div>
+            </div>
+            <div className='flex flex-col xl:items-start items-center'>
+                <h1 className="text-3xl font-bold py-5">Categories</h1>
+                <div className=' text-white gap-4 px-5 pt-5'>
+                    <ul>
+                        <li className='text-bold text-xl font-serif'>Award</li>
+                        <li className='text-bold text-xl font-serif'>Camera</li>
+                        <li className='text-bold text-xl font-serif'>Festival</li>
+                        <li className='text-bold text-xl font-serif'>Video</li>
+                    </ul>
+                </div>
             </div>
         </div>
+
     )
 }
 
--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -4,21 +4,58 @@ import BlogParallaxComponent from '@/components/blog-page-components/BlogParalla
 import BlogRightSection from '@/components/blog-page-components/BlogRightSection/BlogRightSection'
 import Footer from '@/components/footer/Footer'
 import Navbar from '@/components/navbar/Navbar'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const index = () => {
+    const [isFixed, setIsFixed] = useState(false);
+
+    useEffect(() => {
+        const navbar = document.querySelector('.navbar');
+        const navLinksContainer = document.querySelector('.nav-links-container');
+
+        function handleScroll() {
+            if (window.scrollY >= navbar?.offsetHeight) {
+                setIsFixed(true);
+            } else {
+                setIsFixed(false);
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
         <>
             <Navbar />
             <BlogParallaxComponent />
             <div class="grid grid-cols-1 xl:grid-cols-5 gap-4 bg-[#111111] ">
                 <div class="col-span-4">
+                    <div className={`nav-links-container ${isFixed ? 'fixed top-20' : ''} bg-[#111111] w-full py-2 shadow-lg transition duration-500 ease-in-out transform z-50`}>
+                        <div className='text-white flex justify-center items-center flex-wrap gap-5 my-10'>
+                            <div className='py-2 rounded-md cursor-pointer px-6 hover:bg-gray-200 hover:text-[#111111]'>
+                                Popular News
+                            </div>
+                            <div className='py-2 rounded-md cursor-pointer px-6 hover:bg-gray-200 hover:text-[#111111]'>
+                                Guide
+                            </div>
+                            <div className='py-2 rounded-md cursor-pointer px-6 hover:bg-gray-200 hover:text-[#111111]'>
+                                What's New
+                            </div>
+                            <div className='py-2 rounded-md cursor-pointer px-6 hover:bg-gray-200 hover:text-[#111111]'>
+                                Client Feedback
+                            </div>
+                        </div>
+                    </div>
+
                     <AlternateImageContainer />
                     <AlternateImageContainer />
                     <AlternateImageContainer />
                 </div>
                 <div class="col-span-1 text-white">
-                <BlogRightSection/>
+                    <BlogRightSection />
                 </div>
             </div>
             <Footer />

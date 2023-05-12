c8d8cef401242d2c35a8321bc99a73dc5cbcd607
akash-endlos <akash.endlos@gmail.com>
2023-05-12 16:54:12 +0530

    updated

--- src/components/home-page-components/HomeWriteUsContainer/HomeWriteUsContainer.jsx
+++ src/components/home-page-components/HomeWriteUsContainer/HomeWriteUsContainer.jsx
@@ -14,11 +14,11 @@ const HomeWriteUsContainer = () => {
             >
                 <div className='container mx-auto py-10 px-5'>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                        <form className="w-full">
+                        <htmlForm className="w-full">
                             <div className='text-white pb-10'><span className="font-bold tracking-[.2em] cursor-pointer text-2xl align-middle ">Write Us</span></div>
                             <div className="flex flex-wrap -mx-3 mb-6">
                                 <div className="w-full md:w-3/4 px-3 mb-6 md:mb-0">
-                                    <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" for="grid-first-name">
+                                    <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="grid-first-name">
                                         First Name
                                     </label>
                                     <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="please enter full name" />
@@ -26,7 +26,7 @@ const HomeWriteUsContainer = () => {
                             </div>
                             <div className="flex flex-wrap -mx-3 mb-6">
                                 <div className="w-full md:w-3/4 px-3 mb-6 md:mb-0">
-                                    <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" for="grid-first-name">
+                                    <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="grid-first-name">
                                         Email
                                     </label>
                                     <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="please enter email" />
@@ -35,7 +35,7 @@ const HomeWriteUsContainer = () => {
                             <div className="flex flex-wrap -mx-3 mb-6">
                                 <div className="w-full md:w-3/4 px-3 mb-6 md:mb-0">
 
-                                    <label for="message" className="block mb-2 text-sm font-medium text-white">YOUR MESSAGE</label>
+                                    <label htmlFor="message" className="block mb-2 text-sm font-medium text-white">YOUR MESSAGE</label>
                                     <textarea id="message" rows="4" className=" outline-none block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
 
                                 </div>
@@ -44,8 +44,8 @@ const HomeWriteUsContainer = () => {
                                 <button className="bg-white text-black hover:text-white hover:bg-black py-2 px-4 rounded">Submit</button>
 
                             </div>
-                        </form>
-                        <img src="https://images.all-free-download.com/images/graphiclarge/canyon_desert_forest_hill_landscape_mountain_nature_600620.jpg" />
+                        </htmlForm>
+                        <img src="https://images.all-free-download.com/images/graphiclarge/canyon_desert_htmlForest_hill_landscape_mountain_nature_600620.jpg" />
 
                     </div>
                 </div>
--- src/pages/contact-us/write-to-us/index.jsx
+++ src/pages/contact-us/write-to-us/index.jsx
@@ -64,11 +64,11 @@ const index = () => {
                         </div>
 
                         <div>
-                            <form className="w-full mx-auto px-5">
+                            <htmlForm className="w-full mx-auto px-5">
                                 <div className='text-white pb-10'><span className="font-bold tracking-[.2em] cursor-pointer text-2xl align-middle ">Write Us</span></div>
                                 <div className="flex flex-wrap -mx-3 mb-6">
                                     <div className="w-full md:w-3/4 px-3 mb-6 md:mb-0">
-                                        <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" for="grid-first-name">
+                                        <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="grid-first-name">
                                             First Name
                                         </label>
                                         <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="please enter full name" />
@@ -76,7 +76,7 @@ const index = () => {
                                 </div>
                                 <div className="flex flex-wrap -mx-3 mb-6">
                                     <div className="w-full md:w-3/4 px-3 mb-6 md:mb-0">
-                                        <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" for="grid-first-name">
+                                        <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="grid-first-name">
                                             Email
                                         </label>
                                         <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="please enter email" />
@@ -85,7 +85,7 @@ const index = () => {
                                 <div className="flex flex-wrap -mx-3 mb-6">
                                     <div className="w-full md:w-3/4 px-3 mb-6 md:mb-0">
 
-                                        <label for="message" className="block mb-2 text-sm font-medium text-white">YOUR MESSAGE</label>
+                                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-white">YOUR MESSAGE</label>
                                         <textarea id="message" rows="4" className=" outline-none block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
 
                                     </div>
@@ -94,7 +94,7 @@ const index = () => {
                                     <button className="bg-white text-black hover:text-white hover:bg-black py-2 px-4 rounded">Submit</button>
 
                                 </div>
-                            </form>
+                            </htmlForm>
                         </div>
                     </div>
                 </div>

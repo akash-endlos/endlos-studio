89b3bb54fdc878365f225ce18969762f78e1ca2e
akash-endlos <akash.endlos@gmail.com>
2023-05-12 08:59:31 -0700

    updated

--- src/allcontent/navdata.js
+++ src/allcontent/navdata.js
@@ -4,10 +4,12 @@ export const menuItems = [
     {
       main: "Portfolio",
       link:"",
+      id:"",
       children: [
         {
           childmenu: "Archviz",
           link:"",
+          id:"",
           subchildren: [
             {
               subchild: "Walkthrough",
@@ -24,6 +26,7 @@ export const menuItems = [
         {
           childmenu: "Pre Visualization",
           link:"/portfolio/pre-visualization",
+          id:"",
           // subchildren: [
           //   {
           //     subchild: "Pre Production",
@@ -44,6 +47,7 @@ export const menuItems = [
         {
           childmenu: "Virtual production",
           link:"",
+          id:"",
           subchildren: [
             {
               subchild: "Enviorment Design",
@@ -64,12 +68,14 @@ export const menuItems = [
         {
           childmenu: "Game Development",
           link:"/portfolio/game-development",
+          id:"",
         },
       ],
     },
     {
       main: "Blog",
       link:"/blog",
+      id:"",
       // children: [
       //   {
       //     childmenu: "What's New",
@@ -96,18 +102,22 @@ export const menuItems = [
     {
       main: "Company",
       link:"",
+      id:"",
       children: [
         {
           childmenu: "About Us",
           link:"/company/about-us",
+          id:"",
         },
         {
           childmenu: "Careers",
           link:"/company/careers",
+          id:"",
         },
         {
           childmenu: "Services",
           link:"",
+          id:"",
           subchildren: [
             {
               subchild: "Pre Visualization",
@@ -138,18 +148,22 @@ export const menuItems = [
             {
               subchild: "Game Development",
               link:"",
+              id:"",
             },
             {
               subchild: "Archviz",
               link:"",
+              id:"",
               subgrandchildren: [
                 {
                   subchild: "Walkthrough",
                   link:"",
+                  id:"",
                 },
                 {
                   subchild: "AR & VR application",
                   link:"",
+                  id:"",
                 },
               ],
             },
@@ -158,20 +172,24 @@ export const menuItems = [
         {
           childmenu: "Our Vision",
           link:"",
+          id:"",
         },
         {
           childmenu: "FAQ",
           link:"/company/faq",
+          id:"",
         },
       ],
     },
     {
       main: "Contact Us",
       link:"",
+      id:"",
       children: [
         {
           childmenu: "Write to us",
           link:"/contact-us/write-to-us",
+          id:"",
         },
         // {
         //   childmenu: "Book a meeting",

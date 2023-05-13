44dfe4b9c0d4d8a83c0ead4fc6a670175104e193
akash-endlos <akash.endlos@gmail.com>
2023-05-13 09:14:36 -0700

    updated

--- src/pages/company/about-us/index.jsx
+++ src/pages/company/about-us/index.jsx
@@ -7,11 +7,28 @@ import HomeImageContainer from '@/components/home-page-components/HomeImageConta
 import HomeVideoCarousel from '@/components/home-page-components/HomeVideoCarousel/HomeVideoCarousel'
 import Navbar from '@/components/navbar/Navbar'
 import React from 'react'
+import bg from "../../../../public/assets/img1.jpg";
 
 const page = () => {
   return (
     <>
       <Navbar />
+      <div className="relative">
+                <div className="h-screen bg-cover bg-fixed" style={{
+                    backgroundImage: `url(${bg.src})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    width: "100vw",
+                    height: "500px",
+                }}>
+                    <div className="absolute inset-0 bg-black opacity-60"></div>
+                    <div className="absolute inset-0 flex justify-center items-center">
+                        <div className="text-center">
+                            <h1 className="text-5xl font-bold text-white">About Us</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
       <HomeImageContainer />
       <AboutPageCarousel />
       <OurGoal/>

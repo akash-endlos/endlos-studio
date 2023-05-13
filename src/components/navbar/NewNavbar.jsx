40d000aaab6773e4fd46764bfd96ddd2f115a20b
akash-endlos <akash.endlos@gmail.com>
2023-05-13 07:51:25 -0700

    updated

--- src/components/navbar/NewNavbar.jsx
+++ src/components/navbar/NewNavbar.jsx
@@ -4,6 +4,25 @@ import React from "react";
 import { AiFillCaretRight } from "react-icons/ai";
 
 const NewNavbar = () => {
+  const MultiSubNestedMenu = ({ subchildrenMenu }) => (
+    <ul className="absolute right-0 top-0 bg-[#111111] w-[170px] py-2">
+      {subchildrenMenu &&
+        subchildrenMenu.map((item) => (
+          <li className="px-2 py-2 relative cursor-pointer pl-5" key={item.subchild}>
+            {/* <span className="font-semibold text-sm">{item.subchild}</span> */}
+            <div className="flex justify-between">
+              <Link href={{
+                pathname:`${item.link}`,
+                query:{
+                  id:`${item.id}`
+                }
+              }}><div className="font-semibold text-sm">{item.subchild}</div></Link>
+              {item.subgrandchildren && <AiFillCaretRight />}
+            </div>
+          </li>
+        ))}
+    </ul>
+  );
 
   const SubNestedMenu = ({ subchildrenMenu }) => (
     <ul className="absolute right-0 top-0 bg-[#111111] w-[170px] py-2">
@@ -21,7 +40,7 @@ const NewNavbar = () => {
               {item.subgrandchildren && <AiFillCaretRight />}
             </div>
             {item.subgrandchildren && (
-              <SubNestedMenu subchildrenMenu={item.subgrandchildren} />
+              <MultiSubNestedMenu subchildrenMenu={item.subgrandchildren} />
             )}
           </li>
         ))}

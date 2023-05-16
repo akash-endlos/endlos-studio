0d09bd5fb82369617b619f6f139fc7d58cac05a9
akash-endlos <akash.endlos@gmail.com>
2023-05-16 10:09:41 +0530

    updated

--- src/pages/contact-us/write-to-us/index.jsx
+++ src/pages/contact-us/write-to-us/index.jsx
@@ -143,15 +143,14 @@ const index = () => {
           </div>
         </div>
       </div>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15282225.79979123!2d73.7250245393691!3d20.750301298393563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1587818542745!5m2!1sen!2sin"
-        className="max-w-full w-full"
-        height="450"
-        frameborder="0"
-        style={{ border: 0 }}
-        allowfullscreen=""
-        aria-hidden="false"
-        tabindex="0"
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1835.950518465944!2d72.5082235075285!3d23.027405573344772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9b3bf029e7b5%3A0x79f04cd53e9557b!2sEndlos%20Technologies%20Private%20Limited!5e0!3m2!1sen!2sin!4v1684211680037!5m2!1sen!2sin" 
+      className="max-w-full w-full"
+      height="450"
+      frameborder="0"
+      style={{ border: 0 }}
+      allowfullscreen=""
+      aria-hidden="false"
+      tabindex="0"
       ></iframe>
       <Footer />
     </>

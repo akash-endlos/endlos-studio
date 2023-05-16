7cb70be6e206877a367ed945facc3ede347d4a4f
akash-endlos <akash.endlos@gmail.com>
2023-05-16 17:42:16 +0530

    updated

---
+++ src/allcontent/hamBurgerData.js
@@ -0,0 +1,138 @@
+
+
+export const hammenuItems = [
+  {
+    main: "Portfolio",
+    link: "",
+    id: "",
+    children: [
+      {
+        main: "Archviz",
+        link: "",
+        id: "",
+        children: [
+          {
+            main: "Walkthrough",
+            link: "/portfolio/archviz",
+            id: "archvizwalkthrough"
+          },
+          {
+            main: "Interactive Archviz",
+            link: "/portfolio/archviz",
+            id: "archvizinteractive"
+          },
+        ],
+      },
+      {
+        main: "Pre Visualization",
+        link: "/portfolio/pre-visualization",
+        id: "",
+      },
+      {
+        main: "Virtual production",
+        link: "",
+        id: "",
+        children: [
+          {
+            main: "Enviorment Design",
+            link: "/portfolio/virtual-production",
+            id: "environment"
+          },
+          {
+            main: "AR & VR",
+            link: "/portfolio/virtual-production",
+            id: "arvr"
+          },
+        ],
+      },
+      {
+        main: "Game Development",
+        link: "/portfolio/game-development",
+        id: "",
+      },
+    ],
+  },
+  {
+    main: "Blog",
+    link: "/blog",
+    id: "",
+  },
+  {
+    main: "Company",
+    link: "",
+    id: "",
+    children: [
+      {
+        main: "About Us",
+        link: "/company/about-us",
+        id: "",
+      },
+      {
+        main: "Careers",
+        link: "/company/careers",
+        id: "",
+      },
+      {
+        main: "Services",
+        link: "",
+        id: "",
+        children: [
+          {
+            main: "Pre Visualization",
+            link: "/company/services",
+            id: "servicespv"
+          },
+          {
+            main: "Virtual production",
+            link: "/company/services",
+            id: "servicesvp"
+          },
+          {
+            main: "Game Development",
+            link: "/company/services",
+            id: "servicesgamedevelopment",
+          },
+          {
+            main: "Archviz",
+            link: "",
+            id: "",
+            subgrandchildren: [
+              {
+                main: "Walkthrough",
+                link: "/company/services",
+                id: "serviceswalktrough",
+              },
+              {
+                main: "AR & VR application",
+                link: "/company/services",
+                id: "servicesarchviz",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        main: "Our Vision",
+        link: "/company/our-vision",
+        id: "",
+      },
+      {
+        main: "FAQ",
+        link: "/company/faq",
+        id: "",
+      },
+    ],
+  },
+  {
+    main: "Contact Us",
+    link: "",
+    id: "",
+    children: [
+      {
+        main: "Write to us",
+        link: "/contact-us/write-to-us",
+        id: "",
+      },
+    ],
+  },
+];
--- src/components/navbar/HamburgerMenu.jsx
+++ src/components/navbar/HamburgerMenu.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { RxCross2 } from "react-icons/rx";
 import { AiFillCaretDown } from "react-icons/ai";
-import { menuItems, navData } from "@/allcontent/navdata";
+import { hammenuItems } from "@/allcontent/hamBurgerData";
 
 const HamburgerMenu = () => {
   const [toggle, setToggle] = useState(false);
@@ -16,7 +16,7 @@ const HamburgerMenu = () => {
 
     return (
       <div>
-        <button
+        <div
           onClick={toggleExpanded}
           className="flex items-center justify-between w-full py-2 px-4 text-lg font-semibold bg-[#161616] hover:bg-gray-500 focus:bg-gray-500 focus:outline-none"
         >
@@ -25,9 +25,9 @@ const HamburgerMenu = () => {
             className={`h-6 w-6 transform ${isExpanded ? "rotate-180" : ""}`}
             viewBox="0 0 24 24"
           >
-            <AiFillCaretDown />
+            {children && <AiFillCaretDown />}
           </div>
-        </button>
+        </div>
         <div className={`accordion-content ${isExpanded ? "" : "hidden"} pl-6 bg-[#161616]`}>
           {children && children.map((childItem) => (
             <AccordionItem key={childItem.main} title={childItem.main} children={childItem.children} />
@@ -54,8 +54,8 @@ const HamburgerMenu = () => {
       )}
 
       {toggle ? (
-        <div className="fixed top-[90px] z-50 left-0 w-full h-auto max-h-[500px] pb-10 transition duration-100 bg-[#161616] text-white overflow-scroll">
-          {menuItems && menuItems.map((item) => (
+        <div className="fixed top-[90px] z-50 left-0 w-full h-auto max-h-[500px] pb-10 transition duration-100 bg-[#161616] opacity-90 text-white overflow-scroll">
+          {hammenuItems && hammenuItems.map((item) => (
             <AccordionItem key={item.main} title={item.main} children={item.children} />
           ))}
         </div>

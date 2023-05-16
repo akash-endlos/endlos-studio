ac7e62fae00155f4361bdf6caac554d60daa05e4
akash-endlos <akash.endlos@gmail.com>
2023-05-16 15:09:52 +0530

    updated

--- src/components/navbar/NewNavbar.jsx
+++ src/components/navbar/NewNavbar.jsx
@@ -5,10 +5,10 @@ import { AiFillCaretRight } from "react-icons/ai";
 
 const NewNavbar = () => {
   const MultiSubNestedMenu = ({ subchildrenMenu }) => (
-    <ul className="absolute right-0 top-0 bg-[#111111] w-[170px] py-2">
+    <ul className="absolute right-0 top-0  w-[170px] py-2">
       {subchildrenMenu &&
         subchildrenMenu.map((item) => (
-          <li className="px-2 py-2 relative cursor-pointer pl-5" key={item.subchild}>
+          <li className="px-2 py-3 relative cursor-pointer pl-5 bg-[#111111] text-white hover:bg-white hover:text-[#111111] rounded-lg" key={item.subchild}>
             {/* <span className="font-semibold text-sm">{item.subchild}</span> */}
             <Link href={{
                 pathname: `${item.link}`,
@@ -26,10 +26,10 @@ const NewNavbar = () => {
   );
 
   const SubNestedMenu = ({ subchildrenMenu }) => (
-    <ul className="absolute right-0 top-0 bg-[#111111] w-[170px] py-2">
+    <ul className="absolute right-0 top-0  w-[170px] py-2">
       {subchildrenMenu &&
         subchildrenMenu.map((item) => (
-          <li className="px-2 py-2 relative cursor-pointer pl-5" key={item.subchild}>
+          <li className="px-2 py-3 relative cursor-pointer pl-5 bg-[#111111] text-white hover:bg-white hover:text-[#111111] rounded-lg" key={item.subchild}>
             {/* <span className="font-semibold text-sm">{item.subchild}</span> */}
             <Link href={{
                 pathname: `${item.link}`,
@@ -49,9 +49,9 @@ const NewNavbar = () => {
   );
 
   const DropdownMenu = ({ childMenu }) => (
-    <ul className="absolute xl:right-32 md:right-40 top-24 bg-[#111111] w-[170px] py-2">
+    <ul className="absolute xl:right-32 md:right-40 top-24  w-[170px] py-2 ">
       {childMenu.map((child) => (
-        <li className="py-3 relative cursor-pointer px-2 pl-5" key={child.childmenu}>
+        <li className="py-3 relative cursor-pointer px-2 pl-5 bg-[#111111] text-white hover:bg-white hover:text-[#111111] rounded-lg" key={child.childmenu}>
            <Link href={child.link}><div className="flex justify-between">
            <div className="font-semibold text-sm">{child.childmenu}</div>
             {child.subchildren && <AiFillCaretRight />}
@@ -66,7 +66,7 @@ const NewNavbar = () => {
 
   return (
     <div className="hidden xl:block z-20">
-      <ul className="flex justify-between items-center gap-28">
+      <ul className="flex justify-between items-center gap-28 ">
         {menuItems.map((item) => (
           <li className="relative py-10 cursor-pointer" key={item.main}>
             <Link href={item.link}><span className="font-semibold">{item.main}</span></Link>

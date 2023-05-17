6e5497276df6c86a7accca7fa337228db0895843
akash-endlos <akash.endlos@gmail.com>
2023-05-17 12:50:56 +0530

    updated

--- src/components/home-page-components/HomeWriteUsContainer/HomeWriteUsContainer.jsx
+++ src/components/home-page-components/HomeWriteUsContainer/HomeWriteUsContainer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import bg from "../../../../public/assets/home-7-img-9.jpg";
+import bg from "../../../../public/assets/write_us_bg.jpg";
 import FormContainer from '@/components/form-container/FormContainer';
 const HomeWriteUsContainer = () => {
     return (
-        <div className="w-full bg-[#111111] pt-10">
+        <div className="w-full bg-[#111111]">
             <div
                 style={{
                     backgroundImage: `url(${bg.src})`,
@@ -15,9 +15,11 @@ const HomeWriteUsContainer = () => {
             >
                 <div className='container mx-auto py-10 px-5'>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                      <FormContainer/>
-                        <img data-aos="fade-left" data-aos-delay="500"   src="https://images.freeimages.com/images/previews/ac9/railway-hdr-1361893.jpg" />
-
+                        <FormContainer />
+                        {/* <img data-aos="fade-left" data-aos-delay="500"   src="https://images.freeimages.com/images/previews/ac9/railway-hdr-1361893.jpg" /> */}
+                        <div class="relative aspect-video overflow-hidden rounded-3xl">
+                            <video class="w-full" src='../assets/write_us_video.mp4' autoPlay muted></video>
+                        </div>
                     </div>
                 </div>
             </div>

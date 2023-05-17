3ae8c9bbca8b982b6289e1a432a2147ba1adf7e8
akash-endlos <akash.endlos@gmail.com>
2023-05-17 18:33:02 +0530

    updated

--- src/pages/portfolio/game-development/index.jsx
+++ src/pages/portfolio/game-development/index.jsx
@@ -43,8 +43,8 @@ const index = () => {
                     <div className="mx-auto max-w-screen-xl py-5  ">
                         <p className='text-center font-bold text-4xl text-white py-10'>OUR DEVELOPMENT</p>
                         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pb-10">
-                            <div className=" p-4 ">
-                                <img className="w-full  object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
+                        <div className=" p-4 ">
+                                <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
                                 <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">DIRECTOR</h2>
                                 <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                                 <button className="btn text-[#b6b6b6]">Read More</button>

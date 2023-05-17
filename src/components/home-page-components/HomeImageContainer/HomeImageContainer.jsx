bd9c981ab41a4aa233b05cc8333264d1c29a5153
akash-endlos <akash.endlos@gmail.com>
2023-05-17 11:21:14 +0530

    updated

--- src/components/home-page-components/HomeImageContainer/HomeImageContainer.jsx
+++ src/components/home-page-components/HomeImageContainer/HomeImageContainer.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import bg from "../../../../public/assets/home-7-img-9.jpg";
+import bg from "../../../../public/assets/home-1-img.jpg";
 const HomeImageContainer = () => {
   return (
     <div className="bg-[#111111] h-auto relative">
@@ -11,12 +11,13 @@ const HomeImageContainer = () => {
           backgroundRepeat: "no-repeat",
           width: "100vw",
           height: "auto",
+          opacity:0.8
         }}
       >
-        <div className="flex justify-center items-center py-10">
-          <img src="/assets/home-7-img-1.jpg" />
+        <div className="flex justify-beween items-center py-10 px-10">
+          <img src="/assets/home-2-img.jpg" />
         </div>
-        <p className='max-w-screen-lg mx-auto py-10 text-white font-serif text-justify px-5 h-auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolores facere, dignissimos iure nemo voluptas ea voluptates eius rerum. Hic voluptatum quasi illum odio saepe, odit iusto sunt quis quae magni eum maxime aut minima sint aperiam quibusdam atque repudiandae quisquam quia laboriosam cupiditate incidunt fuga! Inventore aperiam eius obcaecati aspernatur recusandae voluptatibus dolores natus, vel, ullam aliquid fugiat optio fuga possimus repellat dolore quod ipsa eligendi ratione quia ad et quasi. Quidem exercitationem velit repudiandae quia numquam ipsum omnis. Quo mollitia dignissimos quam excepturi, adipisci nihil quasi inventore accusamus enim cum ipsam ab earum recusandae harum itaque quae expedita nulla quia iste laudantium, eum vitae! Atque obcaecati libero, porro modi ab hic totam aliquid ipsam at maxime molestias commodi eligendi. Cumque, eos?</p>
+        <p className='max-w-screen-lg mx-auto py-10 text-white font-serif text-justify  h-auto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolores facere, dignissimos iure nemo voluptas ea voluptates eius rerum. Hic voluptatum quasi illum odio saepe, odit iusto sunt quis quae magni eum maxime aut minima sint aperiam quibusdam atque repudiandae quisquam quia laboriosam cupiditate incidunt fuga! Inventore aperiam eius obcaecati aspernatur recusandae voluptatibus dolores natus, vel, ullam aliquid fugiat optio fuga possimus repellat dolore quod ipsa eligendi ratione quia ad et quasi. Quidem exercitationem velit repudiandae quia numquam ipsum omnis. Quo mollitia dignissimos quam excepturi, adipisci nihil quasi inventore accusamus enim cum ipsam ab earum recusandae harum itaque quae expedita nulla quia iste laudantium, eum vitae! Atque obcaecati libero, porro modi ab hic totam aliquid ipsam at maxime molestias commodi eligendi. Cumque, eos?</p>
       </div>
     </div>
   );

15104e6736eb91ed68c031554f41931f9b872012
akash-endlos <akash.endlos@gmail.com>
2023-05-17 13:23:13 +0530

    updated

--- src/components/home-page-components/HomseServices/HomeServices.jsx
+++ src/components/home-page-components/HomseServices/HomeServices.jsx
@@ -7,45 +7,45 @@ const HomeServices = () => {
       <p className='text-center font-bold text-4xl text-white py-10'>SERVICES</p>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 pb-10">
           <div className="bg-[#111111] p-4 text-center">
-            <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
+            <img className="w-full h-48 object-cover mb-2" src="/assets/home_service_1.jpg" alt="Card Image" />
             <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">DIRECTOR</h2>
             <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
           <div className="bg-[#111111] p-4 text-center">
-            <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
+            <img className="w-full h-48 object-cover mb-2" src="/assets/home_service_2.jpg" alt="Card Image" />
             <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">TV SHOWS</h2>
             <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
           <div className="bg-[#111111] p-4 text-center">
-            <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
+            <img className="w-full h-48 object-cover mb-2" src="/assets/home_service_3.jpg" alt="Card Image" />
             <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">THEATER</h2>
             <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
           <div className="bg-[#111111] p-4 text-center">
-            <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
+            <img className="w-full h-48 object-cover mb-2" src="/assets/home_service_4.jpg" alt="Card Image" />
             <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">FILM FESTIVALS</h2>
             <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
           <div className="bg-[#111111] p-4 text-center">
-            <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
+            <img className="w-full h-48 object-cover mb-2" src="/assets/home_service_5.jpg" alt="Card Image" />
             <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">ACADEMY AWARDS</h2>
             <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
           <div className="bg-[#111111] p-4 text-center">
-            <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
+            <img className="w-full h-48 object-cover mb-2" src="/assets/home_service_6.jpg" alt="Card Image" />
             <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">CINEMAS</h2>
             <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
-          <div className="bg-[#111111] p-4 text-center">
-            <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
+          {/* <div className="bg-[#111111] p-4 text-center">
+            <img className="w-full h-48 object-cover mb-2" src="/assets/home_service_2.jpg" alt="Card Image" />
             <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">BLOCK BUSTERS</h2>
             <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
           <div className="bg-[#111111] p-4 text-center">
-            <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
+            <img className="w-full h-48 object-cover mb-2" src="/assets/home_service_2.jpg" alt="Card Image" />
             <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">PRODUCTION</h2>
             <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>

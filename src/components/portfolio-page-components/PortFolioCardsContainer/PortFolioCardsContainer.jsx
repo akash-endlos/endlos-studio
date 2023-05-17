d7f245462f6d0ab9783e1298db58001f1c114423
akash-endlos <akash.endlos@gmail.com>
2023-05-17 18:24:59 +0530

    updated

--- src/components/portfolio-page-components/PortFolioCardsContainer/PortFolioCardsContainer.jsx
+++ src/components/portfolio-page-components/PortFolioCardsContainer/PortFolioCardsContainer.jsx
@@ -4,22 +4,22 @@ const PortFolioCardsContainer = () => {
     return (
         <div className="mx-auto max-w-full p-5  ">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 pb-10 gap-4">
-                <div className=" py-4  " data-aos="fade-up" data-aos-duration="2000">
+                <div className=" py-4  " >
                     <img className="w-full h-48 object-cover mb-2" src="/assets/inner-spa.jpg" alt="Card Image" />
                     <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">Visualization</h2>
                     {/* <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
                 </div>
-                <div className=" py-4 " data-aos="fade-down" data-aos-duration="2000">
+                <div className=" py-4 ">
                     <img className="w-full h-48 object-cover mb-2" src="/assets/LED_live_action.jpg" alt="Card Image" />
                     <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">LED Live Action</h2>
                     {/* <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
                 </div>
-                <div className=" py-4 " data-aos="fade-up" data-aos-duration="2000">
+                <div className=" py-4 ">
                     <img className="w-full h-48 object-cover mb-2" src="/assets/Motion_Capture.jpg" alt="Card Image" />
                     <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">Motion Capture</h2>
                     {/* <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
                 </div>
-                <div className=" py-4 " data-aos="fade-down" data-aos-duration="2000">
+                <div className=" py-4 ">
                     <img className="w-full h-48 object-cover mb-2" src="/assets/Hybrid_camera.jpg" alt="Card Image" />
                     <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">Hybrid Camera</h2>
                     {/* <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}

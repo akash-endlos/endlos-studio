af1391915c873964e9da2a9793ca840d3785d268
akash-endlos <akash.endlos@gmail.com>
2023-05-18 12:09:32 +0530

    updated

--- src/pages/company/faq/index.js
+++ src/pages/company/faq/index.js
@@ -3,7 +3,7 @@ import Footer from '@/components/footer/Footer'
 import Navbar from '@/components/navbar/Navbar'
 import React, { useState } from 'react'
 import { AiOutlineCaretDown } from 'react-icons/ai'
-
+import bg from "../../../../public/assets/img1.jpg";
 
 
 const Index = () => {
@@ -17,7 +17,23 @@ const Index = () => {
 
   return (
     <>
-      <Navbar Navclass="bg-[#111111] text-white" />
+      <Navbar />
+      <div className="relative">
+                <div className="h-screen bg-cover bg-fixed" style={{
+                    backgroundImage: `url(${bg.src})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    width: "100vw",
+                    height: "500px",
+                }}>
+                    <div className="absolute inset-0 bg-black opacity-60"></div>
+                    <div className="absolute inset-0 flex justify-center items-center">
+                        <div className="text-center">
+                            <h1 className="text-5xl font-bold text-white">FAQ</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
       <div className='bg-[#111111] text-white py-5'>
         <div className='pt-24 pb-10'>
           <h1 className='text-center text-2xl'>How Can I Help You ?</h1>

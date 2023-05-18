ed7b6a6798677278ad9a3050cf9fbb5b6cb2fc6f
akash-endlos <akash.endlos@gmail.com>
2023-05-18 10:12:27 +0530

    updated

--- src/allcontent/careers.js
+++ src/allcontent/careers.js
@@ -8,21 +8,21 @@ export const career = [
     },
     {
         id:2,
-        designation: "Unreal Engine Developer",
+        designation: "React Developer",
         description:"The Paint Artist is responsible to remove unwanted artifacts, reconstruct a background or digitally correct a render or composite with attention",
         jobType:"Full Time",
         date:"11/08/2022"
     },
     {
         id:3,
-        designation: "Unreal Engine Developer",
+        designation: "Unity Developer",
         description:"The Paint Artist is responsible to remove unwanted artifacts, reconstruct a background or digitally correct a render or composite with attention",
         jobType:"Full Time",
         date:"11/08/2022"
     },
     {
         id:4,
-        designation: "Unreal Engine Developer",
+        designation: "Maya 3D Developer",
         description:"The Paint Artist is responsible to remove unwanted artifacts, reconstruct a background or digitally correct a render or composite with attention",
         jobType:"Full Time",
         date:"11/08/2022"

d8c7277ea36d189d0debbd078c928b10150e1f3c
akash-endlos <akash.endlos@gmail.com>
2023-05-18 11:07:22 +0530

    updated

--- src/components/form-container/FormContainer.jsx
+++ src/components/form-container/FormContainer.jsx
@@ -41,7 +41,7 @@ const FormContainer = () => {
                     <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="grid-first-name">
                         Name
                     </label>
-                    <input type="text" {...register('name')} className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" placeholder="please enter full name" />
+                    <input type="text" {...register('name')} className="appearance-none block w-full text-white  border rounded py-3 px-4 mb-3 leading-tight focus:outline-none bg-black focus:bg-black" id="grid-first-name" placeholder="please enter full name" />
                     {errors.name && <p className='text-white'>{errors.name.message}</p>}
                 </div>
             </div>
@@ -50,7 +50,7 @@ const FormContainer = () => {
                     <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="grid-first-name">
                         Email
                     </label>
-                    <input type="email" {...register('email')} className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" placeholder="please enter email" />
+                    <input type="email" {...register('email')} className="appearance-none block w-full bg-black text-white border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-black" id="grid-first-name" placeholder="please enter email" />
                     {errors.email && <p className='text-white'>{errors.email.message}</p>}
                 </div>
             </div>
@@ -58,7 +58,7 @@ const FormContainer = () => {
                 <div className="w-full md:w-3/4 px-3 mb-6 md:mb-0">
 
                     <label htmlFor="message" className="block mb-2 text-sm font-medium text-white">YOUR MESSAGE</label>
-                    <textarea name="message" {...register('message')} id="message" rows="4" className=" outline-none block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+                    <textarea name="message" {...register('message')} id="message" rows="4" className=" outline-none block p-2.5 w-full text-sm text-white bg-black rounded-lg border border-gray-300  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white  " placeholder="Write your thoughts here..."></textarea>
                     {errors.message && <p className='text-white'>{errors.message.message}</p>}
                 </div>
             </div>

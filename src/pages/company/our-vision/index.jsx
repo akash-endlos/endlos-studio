aa3288437d61063d05a350652980191425c72be0
akash-endlos <akash.endlos@gmail.com>
2023-05-18 12:19:53 +0530

    updated

--- src/pages/company/our-vision/index.jsx
+++ src/pages/company/our-vision/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import bg from "../../../../public/assets/img1.jpg";
+import bg from "../../../../public/assets/our_vision.jpg";
 import Navbar from '@/components/navbar/Navbar';
 import Footer from '@/components/footer/Footer';
 import { FiUserCheck, FiThumbsUp } from "react-icons/fi";
@@ -14,10 +14,10 @@ const index = () => {
             <div className="relative">
                 <div className="h-screen bg-cover bg-fixed" style={{
                     backgroundImage: `url(${bg.src})`,
-                    backgroundSize: "cover",
+                    backgroundSize: "contain",
                     backgroundRepeat: "no-repeat",
                     width: "100vw",
-                    height: "500px",
+                    height: "495px",
                 }}>
                     <div className="absolute inset-0 bg-black opacity-60"></div>
                     <div className="absolute inset-0 flex justify-center items-center">

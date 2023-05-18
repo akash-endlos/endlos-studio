9db1d91265d0150920b517fda432b2e909ba3cc0
akash-endlos <akash.endlos@gmail.com>
2023-05-18 12:34:21 +0530

    updated

--- src/components/portfolio-page-components/PortFolioCardsContainer/PortFolioCardsContainer.jsx
+++ src/components/portfolio-page-components/PortFolioCardsContainer/PortFolioCardsContainer.jsx
@@ -5,7 +5,7 @@ const PortFolioCardsContainer = () => {
         <div className="mx-auto max-w-full p-5  ">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 pb-10 gap-4">
                 <div className=" py-4  " >
-                    <img className="w-full h-48 object-cover mb-2" src="/assets/inner-spa.jpg" alt="Card Image" />
+                    <img className="w-full h-48 object-cover mb-2" src="/assets/inner-space.jpg" alt="Card Image" />
                     <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">Visualization</h2>
                     {/* <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
                 </div>

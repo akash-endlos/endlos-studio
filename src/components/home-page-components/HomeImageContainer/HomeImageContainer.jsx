4bbc4238229c77e816846d354cfe6195bed789ff
akash-endlos <akash.endlos@gmail.com>
2023-05-18 13:26:31 +0530

    updated

--- src/components/home-page-components/HomeImageContainer/HomeImageContainer.jsx
+++ src/components/home-page-components/HomeImageContainer/HomeImageContainer.jsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import React from "react";
 import bg from "../../../../public/assets/home-1-img.jpg";
+import { Typewriter } from "react-simple-typewriter";
+import { useTypewriter } from "react-simple-typewriter";
+import Link from "next/link";
 const HomeImageContainer = () => {
   return (
     <div className="bg-[#111111] h-auto relative">
@@ -17,12 +20,30 @@ const HomeImageContainer = () => {
         <div className="flex justify-end items-center py-10 " >
           <img className="opacity-100" src="/assets/home-2-img.jpg" data-aos="fade-left" data-aos-duration="2000" />
         </div>
+        <h1 className='flex justify-start max-w-4xl px-10 py-2 text-white font-semibold text-4xl text-justify  h-auto'>
+          <Typewriter
+            words={[" About Endlos"]}
+            loop
+            cursor
+            cursorStyle="|"
+            typeSpeed={120}
+            deleteSpeed={70}
+            delaySpeed={1000}
+          />
+        </h1>
         <p data-aos="fade-right" data-aos-duration="2000" className='flex justify-start max-w-4xl px-10 py-2 text-white font-semibold text-xl text-justify  h-auto'>Welcome to Endlos Studio, your destination for cutting-edge Unreal Engine experiences. As a passionate team of creators and innovators, we specialize in harnessing the power of Unreal Engine to bring your wildest imaginations to life.
         </p>
         <p data-aos="fade-left" data-aos-duration="2000" className='flex justify-start max-w-4xl px-10 py-2 text-white font-semibold text-xl text-justify  h-auto'>At Endlos Studio, we believe in the limitless possibilities that Unreal Engine offers. Whether you dream of immersive virtual reality environments, jaw-dropping visual effects, or captivating gameplay mechanics, our skilled artists and developers are here to make it happen.
         </p>
-        <p data-aos="fade-right" data-aos-duration="2000" className='flex justify-start max-w-4xl px-10 py-2 text-white font-semibold text-xl text-justify  h-auto'>With years of experience in the industry, our talented team has honed their craft to deliver stunning visuals, seamless gameplay, and unparalleled attention to detail.
+        <p data-aos="fade-right" data-aos-duration="2000" className='flex justify-start max-w-4xl px-10 pb-10 text-white font-semibold text-xl text-justify  h-auto'>With years of experience in the industry, our talented team has honed their craft to deliver stunning visuals, seamless gameplay, and unparalleled attention to detail.
         </p>
+        <div className='flex justify-start max-w-4xl px-10 pb-10 text-white font-semibold text-xl text-justify  h-auto'>
+       <Link href='/company/about-us'>
+       <button className="mt-4 bg-black hover:bg-white text-white hover:text-black px-4 py-2 rounded-md">
+         Read More
+        </button>
+       </Link>
+        </div>
       </div>
     </div>
   );

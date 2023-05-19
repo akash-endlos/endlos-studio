a2481f563888e45b22178c322b89f6b47d3f187e
akash-endlos <akash.endlos@gmail.com>
2023-05-19 15:45:05 +0530

    updated

--- src/components/about-page-components/OurGoalComponent/OurGoal.jsx
+++ src/components/about-page-components/OurGoalComponent/OurGoal.jsx
@@ -4,7 +4,6 @@ const OurGoal = () => {
   return (
     <div className="bg-[#111111]">
       <div className='flex flex-col gap-5 justify-center items-center text-white py-20'>
-        <h1 className='text-4xl font-bold'>OUR GOAL</h1>
         <p className='text-xl max-w-screen-xl leading-10 text-justify font-serif'>At Endlos Studio, we are a team of passionate professionals who deliver exceptional creative solutions in Virtual Production, Architecture Visualization, Interactive experiences, and Game Development. Our expertise in cutting-edge technologies and our commitment to quality make us strive to bring imagination to life.</p>
         <p className='text-xl max-w-screen-xl leading-10  text-justify font-serif'>Our multidisciplinary team comprises talented artists, designers, developers, and engineers collaborating seamlessly to transform ideas into captivating and immersive experiences. We believe in the power of innovation and constantly push the boundaries of what is possible.</p>
         <p className='text-xl max-w-screen-xl leading-10  text-justify font-serif'>Whether creating realistic virtual environments, crafting photorealistic architectural visualizations, developing engaging interactive applications, or designing captivating games, we are driven by a commitment to excellence. We understand the importance of client collaboration and aim to exceed expectations by delivering exceptional results that inspire and engage audiences.</p>

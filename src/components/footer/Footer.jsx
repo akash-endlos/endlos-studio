822758405abb53fe4fea93924c961733fc6fe5f8
akash-endlos <akash.endlos@gmail.com>
2023-05-19 13:05:05 +0530

    updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,6 +11,7 @@ import {
 const Footer = () => {
   return (
 
+   <>
     <footer class="bg-gray-900 text-white">
       <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
@@ -78,7 +79,90 @@ const Footer = () => {
         </div>
       </div>
     </footer>
+    <hr/>
+    <footer className="bg-gray-900 px-10">
+      <div className="mx-6 py-10 text-center md:text-left text-white">
+        <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
+          <div className="">
+            <img src="/assets/logo.png" className="w-60 py-2" />
+            <p className="max-w-xs text-justify">
+              Endlos Studio is a leading creative studio that pushes digital innovation's boundaries. With a team of talented artists, developers, and technologists, we create awe-inspiring experiences in various domains, such as virtual production, architectural visualization, interactive storytelling, and game development.
+            </p>
+          </div>
+          <div className="text-white">
+            <h6
+              className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+              Products
+            </h6>
+            <p className="mb-4">
+              <a
+              >Angular</a
+              >
+            </p>
+            <p className="mb-4">
+              <a
+              >React</a
+              >
+            </p>
+            <p className="mb-4">
+              <a
+              >Vue</a
+              >
+            </p>
+            <p>
+              <a
+              >Laravel</a
+              >
+            </p>
+          </div>
+          <div className="text-white">
+            <h6
+              className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+              Useful links
+            </h6>
+            <p className="mb-4">
+              <a
+              >Pricing</a
+              >
+            </p>
+            <p className="mb-4">
+              <a
+              >Settings</a
+              >
+            </p>
+            <p className="mb-4">
+              <a
+              >Orders</a
+              >
+            </p>
+            <p>
+              <a
+              >Help</a
+              >
+            </p>
+          </div>
+          <div>
+            <h6
+              className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+              Contact
+            </h6>
+            <p className="mb-4 flex items-center justify-center md:justify-start gap-2">
+              <AiFillMail />
+              info@example.com
+            </p>
+            <p className="mb-4 flex items-center justify-center md:justify-start gap-2">
+              <BsFillTelephoneFill />
+              + 01 234 567 88
+            </p>
+          </div>
+        </div>
+      </div>
 
+      <div className="text-white py-6 text-center">
+        <span>   © 2023 Endlos Studio. All Rights Reserved.</span>
+      </div>
+    </footer>
+   </>
   );
 };
 

1737e1cb2476510856b1a3de4a03f09d6f457282
akash-endlos <akash.endlos@gmail.com>
2023-05-19 10:25:28 +0530

    updated

--- src/allcontent/blog.js
+++ src/allcontent/blog.js
@@ -1,17 +1,17 @@
 export const blog = [
     {
         id:1,
-        name: "Noteworthy technology acquisitions 2021",
+        name: "Make Our World Beautiful With Virtual Reality",
         value:"left"
     },
     {
         id:2,
-        name: "Noteworthy technology acquisitions 2021",
+        name: "The Impact of Virtual Reality on Architecture and Design",
         value:"right"
     },
     {
         id:3,
-        name: "Noteworthy technology acquisitions 2021",
+        name: "The Unreal Engine 5.2 is available!",
         value:"left"
     },
 ]

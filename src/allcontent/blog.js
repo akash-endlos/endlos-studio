c14f068e01d6d90aab570edd789ce5f83bbfbd47
akash-endlos <akash.endlos@gmail.com>
2023-05-19 16:24:21 +0530

    updated

--- src/allcontent/blog.js
+++ src/allcontent/blog.js
@@ -2,16 +2,19 @@ export const blog = [
     {
         id:1,
         name: "Make Our World Beautiful With Virtual Reality",
-        value:"left"
+        value:"left",
+        src:"/assets/blog_home_vr_1.jpg"
     },
     {
         id:2,
         name: "The Impact of Virtual Reality on Architecture and Design",
-        value:"right"
+        value:"right",
+        src:"/assets/blog_home_vr_2.jpg"
     },
     {
         id:3,
         name: "The Unreal Engine 5.2 is available!",
-        value:"left"
+        value:"left",
+        src:"/assets/blog_home_vr_3.jpg"
     },
 ]
--- src/components/home-page-components/HomeBlogImages/HomeBlogImages.jsx
+++ src/components/home-page-components/HomeBlogImages/HomeBlogImages.jsx
@@ -9,7 +9,7 @@ const HomeBlogImages = () => {
                <Link href='/blog/1'>
                <div className="relative">
                     <div className="card cursor-pointer">
-                        <img src="/assets/home_blog_1.jpg" className="w-full h-[700px]" alt="example image" />
+                        <img src="/assets/home_vr_1.jpg" className="w-full h-[700px]" alt="example image" />
                         <div className="absolute inset-0 bg-black bg-opacity-50 text-white flex items-end justify-start opacity-0 transition duration-300 ease-in-out hover:opacity-100">
                             <span className="text-4xl font-bold absolute bottom-16 left-10">Make Our World Beautiful With Virtual Reality</span>
                         </div>
@@ -21,7 +21,7 @@ const HomeBlogImages = () => {
                    <Link href='/blog/2'>
                    <div className="relative">
                         <div className="card cursor-pointer">
-                            <img src="/assets/home_blog_2.jpg" className="w-full h-[400px]" alt="example image" />
+                            <img src="/assets/home_vr_2.jpg" className="w-full h-[400px]" alt="example image" />
                             <div className="absolute inset-0 bg-black bg-opacity-50 text-white flex items-center justify-center opacity-0 transition duration-300 ease-in-out hover:opacity-100">
                                 <span className="text-4xl font-bold absolute bottom-16 left-10">The Impact of Virtual Reality on Architecture and Design</span>
                             </div>
@@ -31,7 +31,7 @@ const HomeBlogImages = () => {
                     <div className="relative">
                         <Link href='/blog/3'>
                         <div className="card cursor-pointer">
-                            <img src="/assets/home_blog_3.jpg" className="w-full h-[400px]" alt="example image" />
+                            <img src="/assets/home_vr_3.jpg" className="w-full h-[400px]" alt="example image" />
                             <div className="absolute inset-0 bg-black bg-opacity-50 text-white flex items-center justify-center opacity-0 transition duration-300 ease-in-out hover:opacity-100">
                                 <span className="text-4xl font-bold absolute bottom-16 left-10">The Unreal Engine 5.2 is available!</span>
                             </div>

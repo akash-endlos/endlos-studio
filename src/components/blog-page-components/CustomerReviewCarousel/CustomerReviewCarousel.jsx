de1d778ed7ea5f44a8b8501a2886573b36893578
akash-endlos <akash.endlos@gmail.com>
2023-05-19 20:35:41 -0700

    updated

--- src/components/blog-page-components/CustomerReviewCarousel/CustomerReviewCarousel.jsx
+++ src/components/blog-page-components/CustomerReviewCarousel/CustomerReviewCarousel.jsx
@@ -4,7 +4,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper";
-import { AiFillStar } from 'react-icons/ai'
+import { AiFillStar } from "react-icons/ai";
 
 export default function CustomerReviewCarousel() {
   return (
@@ -22,58 +22,71 @@ export default function CustomerReviewCarousel() {
         modules={[Autoplay, Pagination, Navigation]}
         className="max-w-5xl mx-auto"
       >
-          <SwiperSlide className="text-center max-w-screen-xl mx-auto">
-      <div className="flex flex-col justify-center items-center">
-        <div className="flex justify-center items-center">
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-        </div>
-        <h1 className="py-5 text-2xl font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-        <p className="text-xl font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse similique temporibus nam!</p>
-      </div>
-    </SwiperSlide>
-    <SwiperSlide className="text-center max-w-screen-xl mx-auto">
-      <div className="flex flex-col justify-center items-center">
-        <div className="flex justify-center items-center">
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-        </div>
-        <h1 className="py-5 text-2xl font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-        <p className="text-xl font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse similique temporibus nam!</p>
-      </div>
-    </SwiperSlide>
-    <SwiperSlide className="text-center max-w-screen-xl mx-auto">
-      <div className="flex flex-col justify-center items-center">
-        <div className="flex justify-center items-center">
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-        </div>
-        <h1 className="py-5 text-2xl font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-        <p className="text-xl font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse similique temporibus nam!</p>
-      </div>
-    </SwiperSlide>
-    <SwiperSlide className="text-center max-w-screen-xl mx-auto">
-      <div className="flex flex-col justify-center items-center">
-        <div className="flex justify-center items-center">
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-        </div>
-        <h1 className="py-5 text-2xl font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-        <p className="text-xl font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse similique temporibus nam!</p>
-      </div>
-    </SwiperSlide>
+        <SwiperSlide className="text-center max-w-screen-xl mx-auto">
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex justify-center items-center">
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+            </div>
+            {/* <h1 className="py-5 text-2xl font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1> */}
+            <p className="text-xl font-serif max-w-lg py-2">
+              Endlos Studio's work exceeded our expectations. Their attention to
+              detail and commitment to quality made our project shine.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="text-center max-w-screen-xl mx-auto">
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex justify-center items-center">
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+            </div>
+            {/* <h1 className="py-5 text-2xl font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1> */}
+            <p className="text-xl font-serif max-w-lg py-2">
+              Working with Endlos Studio was a delight. They were responsive,
+              professional, and delivered exceptional results.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="text-center max-w-screen-xl mx-auto">
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex justify-center items-center">
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+            </div>
+            {/* <h1 className="py-5 text-2xl font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1> */}
+            <p className="text-xl font-serif max-w-lg py-2">
+              The team at Endlos Studio is highly skilled and creative. They
+              brought our vision to life with their innovative solutions.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="text-center max-w-screen-xl mx-auto">
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex justify-center items-center">
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+            </div>
+            {/* <h1 className="py-5 text-2xl font-serif">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1> */}
+            <p className="text-xl font-serif max-w-lg py-2">
+              Choosing Endlos Studio was one of the best decisions we made.
+              Their professionalism and dedication made the entire process
+              smooth and enjoyable.
+            </p>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
--- src/pages/company/services/index.jsx
+++ src/pages/company/services/index.jsx
@@ -47,7 +47,7 @@ const index = () => {
           <div className="bg-[#111111]">
             <div className='flex flex-col gap-5 justify-center items-center text-white py-20'>
               <h1 className='text-4xl font-bold'>What We Provide</h1>
-              <p className='text-xl max-w-screen-lg text-center font-serif'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut numquam non ea vero quas, libero voluptatibus atque ad asperiores magni</p>
+              {/* <p className='text-xl max-w-screen-lg text-center font-serif'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut numquam non ea vero quas, libero voluptatibus atque ad asperiores magni</p> */}
             </div>
           </div>
         </div>

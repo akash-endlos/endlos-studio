f0c56589c281b4969fcf0feb9b940cc2d8f5085f
akash-endlos <akash.endlos@gmail.com>
2023-05-20 08:37:44 -0700

    updated

--- src/components/navbar/HamburgerMenu.jsx
+++ src/components/navbar/HamburgerMenu.jsx
@@ -3,80 +3,74 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import { RxCross2 } from "react-icons/rx";
 import { AiFillCaretDown } from "react-icons/ai";
 import { hammenuItems } from "@/allcontent/hamBurgerData";
+import { menuItems } from "@/allcontent/navdata";
 
 const HamburgerMenu = () => {
-    const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
-    function AccordionItem({ title, children }) {
-        const [isExpanded, setIsExpanded] = useState(false);
+  return (
+    <div className="lg:block xl:hidden 2xl:hidden">
+      {!toggle ? (
+        <RxHamburgerMenu
+          className="cursor-pointer"
+          size={25}
+          onClick={() => setToggle(!toggle)}
+        />
+      ) : (
+        <RxCross2
+          className="cursor-pointer"
+          size={25}
+          onClick={() => setToggle(!toggle)}
+        />
+      )}
 
-        function toggleExpanded() {
-            setIsExpanded(!isExpanded);
-        }
-
-        return (
-            <div>
-                <div
-                    onClick={toggleExpanded}
-                    className="flex items-center justify-between w-full py-2 px-4 text-lg font-semibold bg-[#161616] hover:bg-gray-500 focus:bg-gray-500 focus:outline-none"
-                >
-                    <span>{title}</span>
-                    <div
-                        className={`h-6 w-6 transform ${isExpanded ? "rotate-180" : ""}`}
-                        viewBox="0 0 24 24"
-                    >
-                        {children && <AiFillCaretDown />}
-                    </div>
+      {toggle ? (
+        <div className="fixed top-[90px] z-50 left-0 w-full h-auto max-h-[500px] pb-10 transition duration-100 bg-[#161616] opacity-90 text-white overflow-scroll">
+          {menuItems.map((item, index) => (
+              <div className=" px-5">
+                <div className="flex justify-between items-center py-4 px-5 relative">
+                  <span>{item.main}</span>
+                  {item.children && <AiFillCaretDown />}
                 </div>
-                <div className={`accordion-content ${isExpanded ? "" : "hidden"} pl-6 bg-[#161616]`}>
-                    {children && children.map((childItem) => (
-                        <AccordionItem key={childItem.main} title={childItem.main} children={childItem.children} />
+                <div className=" w-full  py-4 px-5">
+                  {item.children && item?.children?.map((subitem) => (
+                    <>
+                    <div className="flex justify-between items-center">
+                      <span className="py-4">{subitem.childmenu}</span>
+                      {subitem.subchildren && <AiFillCaretDown />}
+                    </div>
+                    {subitem?.subchildren?.map((subChildren)=>(
+                        <div className="flex justify-between items-center">
+                        <span className="py-4">{subChildren.subchild}</span>
+                        {/* <AiFillCaretDown /> */}
+                      </div>
                     ))}
+                    </>
+                  ))}
                 </div>
-            </div>
-        );
-    }
-
-    return (
-        <div className="lg:block xl:hidden 2xl:hidden">
-            {!toggle ? (
-                <RxHamburgerMenu
-                    className="cursor-pointer"
-                    size={25}
-                    onClick={() => setToggle(!toggle)}
-                />
-            ) : (
-                <RxCross2
-                    className="cursor-pointer"
-                    size={25}
-                    onClick={() => setToggle(!toggle)}
-                />
-            )}
-
-            {toggle ? (
-                <div className="fixed top-[90px] z-50 left-0 w-full h-auto max-h-[500px] pb-10 transition duration-100 bg-[#161616] opacity-90 text-white overflow-scroll">
-                    <div className="relative flex justify-between  items-center my-2 mx-1 px-5 py-5">
-                        <div><span>asdsa</span>
-                            <div className="absolute w-full flex justify-between items-center left-0 py-5 px-5">
-                                <div><span>asdsa</span>
-                                    <div className="absolute w-full flex justify-between items-center left-0 py-5 px-5">
-                                        <div><span>asdsa</span>
-
-                                        </div>
-                                        <div><AiFillCaretDown /></div>
-                                    </div>
-                                </div>
-                                <div><AiFillCaretDown /></div>
-                            </div>
-                        </div>
-                        <div><AiFillCaretDown /></div>
-                    </div>
-                </div>
-            ) : (
-                null
-            )}
+              </div>
+          ))}
         </div>
-    );
+      ) : null}
+    </div>
+  );
 };
 
 export default HamburgerMenu;
+
+{
+  /* <div className="relative px-5">
+          <div className="flex justify-between items-center py-4 px-5 relative">
+            <span>asd</span>
+            <AiFillCaretDown />
+            <div className="absolute top-10 w-full flex justify-between items-center py-4 px-5">
+              <span>asda1</span>
+              <AiFillCaretDown />
+              <div className="absolute top-10 w-full flex justify-between items-center py-4 px-10">
+              <span>asda1</span>
+              <AiFillCaretDown />
+            </div>
+            </div>
+          </div>
+          </div> */
+}
--- src/components/navbar/HamburgerMenudemo.jsx
+++ src/components/navbar/HamburgerMenudemo.jsx
@@ -1,69 +1,133 @@
-import React, { useState } from "react";
-import { RxHamburgerMenu } from "react-icons/rx";
-import { RxCross2 } from "react-icons/rx";
-import { AiFillCaretDown } from "react-icons/ai";
-import { hammenuItems } from "@/allcontent/hamBurgerData";
+// import React, { useState } from "react";
+// import { RxHamburgerMenu } from "react-icons/rx";
+// import { RxCross2 } from "react-icons/rx";
+// import { AiFillCaretDown } from "react-icons/ai";
+// import { hammenuItems } from "@/allcontent/hamBurgerData";
 
-const HamburgerMenu = () => {
-  const [toggle, setToggle] = useState(false);
+// const HamburgerMenu = () => {
+//   const [toggle, setToggle] = useState(false);
 
-  function AccordionItem({ title, children }) {
-    const [isExpanded, setIsExpanded] = useState(false);
+//   function AccordionItem({ title, children }) {
+//     const [isExpanded, setIsExpanded] = useState(false);
 
-    function toggleExpanded() {
-      setIsExpanded(!isExpanded);
-    }
+//     function toggleExpanded() {
+//       setIsExpanded(!isExpanded);
+//     }
 
-    return (
-      <div>
-        <div
-          onClick={toggleExpanded}
-          className="flex items-center justify-between w-full py-2 px-4 text-lg font-semibold bg-[#161616] hover:bg-gray-500 focus:bg-gray-500 focus:outline-none"
-        >
-          <span>{title}</span>
-          <div
-            className={`h-6 w-6 transform ${isExpanded ? "rotate-180" : ""}`}
-            viewBox="0 0 24 24"
-          >
-            {children && <AiFillCaretDown />}
-          </div>
-        </div>
-        <div className={`accordion-content ${isExpanded ? "" : "hidden"} pl-6 bg-[#161616]`}>
-          {children && children.map((childItem) => (
-            <AccordionItem key={childItem.main} title={childItem.main} children={childItem.children} />
-          ))}
-        </div>
-      </div>
-    );
-  }
+//     return (
+//       <div>
+//         <div
+//           onClick={toggleExpanded}
+//           className="flex items-center justify-between w-full py-2 px-4 text-lg font-semibold bg-[#161616] hover:bg-gray-500 focus:bg-gray-500 focus:outline-none"
+//         >
+//           <span>{title}</span>
+//           <div
+//             className={`h-6 w-6 transform ${isExpanded ? "rotate-180" : ""}`}
+//             viewBox="0 0 24 24"
+//           >
+//             {children && <AiFillCaretDown />}
+//           </div>
+//         </div>
+//         <div className={`accordion-content ${isExpanded ? "" : "hidden"} pl-6 bg-[#161616]`}>
+//           {children && children.map((childItem) => (
+//             <AccordionItem key={childItem.main} title={childItem.main} children={childItem.children} />
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   }
 
-  return (
-    <div className="lg:block xl:hidden 2xl:hidden">
-      {!toggle ? (
-        <RxHamburgerMenu
-          className="cursor-pointer"
-          size={25}
-          onClick={() => setToggle(!toggle)}
-        />
-      ) : (
-        <RxCross2
-          className="cursor-pointer"
-          size={25}
-          onClick={() => setToggle(!toggle)}
-        />
-      )}
+//   return (
+//     <div className="lg:block xl:hidden 2xl:hidden">
+//       {!toggle ? (
+//         <RxHamburgerMenu
+//           className="cursor-pointer"
+//           size={25}
+//           onClick={() => setToggle(!toggle)}
+//         />
+//       ) : (
+//         <RxCross2
+//           className="cursor-pointer"
+//           size={25}
+//           onClick={() => setToggle(!toggle)}
+//         />
+//       )}
 
-      {toggle ? (
-        <div className="fixed top-[90px] z-50 left-0 w-full h-auto max-h-[500px] pb-10 transition duration-100 bg-[#161616] opacity-90 text-white overflow-scroll">
-          {hammenuItems && hammenuItems.map((item) => (
-            <AccordionItem key={item.main} title={item.main} children={item.children} />
-          ))}
-        </div>
-      ) : (
-        null
-      )}
-    </div>
-  );
-};
+//       {toggle ? (
+//         <div className="fixed top-[90px] z-50 left-0 w-full h-auto max-h-[500px] pb-10 transition duration-100 bg-[#161616] opacity-90 text-white overflow-scroll">
+//           {hammenuItems && hammenuItems.map((item) => (
+//             <AccordionItem key={item.main} title={item.main} children={item.children} />
+//           ))}
+//         </div>
+//       ) : (
+//         null
+//       )}
+//     </div>
+//   );
+// };
 
-export default HamburgerMenu;
+// export default HamburgerMenu;
+
+
+
+// import React, { useState } from "react";
+// import { RxHamburgerMenu } from "react-icons/rx";
+// import { RxCross2 } from "react-icons/rx";
+// import { AiFillCaretDown } from "react-icons/ai";
+// import { hammenuItems } from "@/allcontent/hamBurgerData";
+// import { menuItems } from "@/allcontent/navdata";
+
+// const HamburgerMenu = () => {
+//   const [toggle, setToggle] = useState(false);
+
+//   return (
+//     <div className="lg:block xl:hidden 2xl:hidden">
+//       {!toggle ? (
+//         <RxHamburgerMenu
+//           className="cursor-pointer"
+//           size={25}
+//           onClick={() => setToggle(!toggle)}
+//         />
+//       ) : (
+//         <RxCross2
+//           className="cursor-pointer"
+//           size={25}
+//           onClick={() => setToggle(!toggle)}
+//         />
+//       )}
+
+//       {toggle ? (
+//         <div className="fixed top-[90px] z-50 left-0 w-full h-auto max-h-[500px] pb-10 transition duration-100 bg-[#161616] opacity-90 text-white overflow-scroll">
+//           {menuItems.map((item, index) => (
+//               <div className=" px-5">
+//                 <div className="flex justify-between items-center py-4 px-5 relative">
+//                   <span>{item.main}</span>
+//                   {item.children && <AiFillCaretDown />}
+//                 </div>
+//                 <div className=" w-full  py-4 px-5">
+//                   {item.children && item?.children?.map((subitem) => (
+//                     <>
+//                     <div className="flex justify-between items-center">
+//                       <span className="py-4">{subitem.childmenu}</span>
+//                       {subitem.subchildren && <AiFillCaretDown />}
+//                     </div>
+//                     {subitem?.subchildren?.map((subChildren)=>(
+//                         <div className="flex justify-between items-center">
+//                         <span className="py-4">{subChildren.subchild}</span>
+//                         {/* <AiFillCaretDown /> */}
+//                       </div>
+//                     ))}
+//                     </>
+//                   ))}
+//                 </div>
+//               </div>
+//           ))}
+//         </div>
+//       ) : null}
+//     </div>
+//   );
+// };
+
+// export default HamburgerMenu;
+
+

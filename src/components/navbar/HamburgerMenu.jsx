2ef4104bb131f9c3f1b00baa18b78d9a30d6c5e6
akash-endlos <akash.endlos@gmail.com>
2023-05-20 12:39:42 -0700

    updated

--- src/components/navbar/HamburgerMenu.jsx
+++ src/components/navbar/HamburgerMenu.jsx
@@ -1,41 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { RxCross2 } from "react-icons/rx";
-import { AiFillCaretUp,AiFillCaretDown } from "react-icons/ai";
+import { AiFillCaretDown } from "react-icons/ai";
 import { hammenuItems } from "@/allcontent/hamBurgerData";
-import Link from "next/link";
 
 const HamburgerMenu = () => {
   const [toggle, setToggle] = useState(false);
+  const [IsExpand, setIsExpand] = useState({portfolio:false,contact:false,company:false});
+  const [IsExpandChild, setIsExpandChild] = useState({parchviz:false});
+  const [DropDownChild, setDropDownChild] = useState("");
+  const [DropDown, setDropDown] = useState("");
 
-  const AccordionItem = ({ title, children,link }) => {
-    const [isOpen, setIsOpen] = useState(false);
+  const handleActiveDropDown=(item,e)=>{
+  e.preventDefault()
+   if(item==='portfolio')
+   {
+    setIsExpand({...IsExpand,portfolio:!IsExpand.portfolio})
+    setDropDown(item)
+   }
+   if(item==='contact')
+   {
+    setIsExpand({...IsExpand,contact:!IsExpand.contact})
+    setDropDown(item)
+   }
+   if(item==='company')
+   {
+    setIsExpand({...IsExpand,company:!IsExpand.company})
+    setDropDown(item)
+   }
+  }
+  const handleActiveChildDropDown=(item,e)=>{
+     if(item==='parchviz')
+     {
+      setIsExpandChild({...IsExpandChild,parchviz:!IsExpandChild.parchviz})
+      setDropDownChild(item)
+     }
 
-    const toggleAccordion = () => {
-      setIsOpen(!isOpen);
-    };
-
-    return (
-      <div onClick={toggleAccordion} className="accordion-item">
-        <div className="accordion-header flex justify-between py-5">
-          <Link href={link}>
-          <div
-            className="accordion-button font-semibold hover:text-gray-200 "
-            type="button"
-           
-          >
-            {title}
-          </div>
-          </Link>
-          <div>
-           {!isOpen ? <AiFillCaretDown onClick={toggleAccordion}/> :<AiFillCaretUp onClick={toggleAccordion}/>}
-          </div>
-        </div>
-        {isOpen && <div className="accordion-content">{children}</div>}
-      </div>
-    );
-  };
-
+    }
+    console.log(IsExpandChild);
   return (
     <div className="lg:block xl:hidden 2xl:hidden">
       {!toggle ? (
@@ -54,32 +56,30 @@ const HamburgerMenu = () => {
 
       {toggle ? (
         <div className="fixed top-[90px] z-50 left-0 w-full h-auto max-h-[300px] pb-10 transition duration-100 bg-[#161616] opacity-90 text-white overflow-scroll">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-5">
-            <div className="accordion">
-              {hammenuItems.map((item) => (
-                <AccordionItem title={item.main} link={item.link}>
-                  {item?.children &&
-                    item?.children?.map((subitem) => (
-                      <AccordionItem title={subitem.childmenu} link={subitem.link}>
-                        {subitem?.subchildren?.map((subchildmenu) => (
-                          <AccordionItem title={subchildmenu.subchild} link={subchildmenu.link}>
-                            {subchildmenu?.subgrandchildren &&
-                              subchildmenu?.subgrandchildren?.map(
-                                (subgrandmenu) => (
-                                  <AccordionItem
-                                    link={subgrandmenu.link}
-                                    title={subgrandmenu.subchild}
-                                  ></AccordionItem>
-                                )
-                              )}
-                          </AccordionItem>
-                        ))}
-                      </AccordionItem>
-                    ))}
-                </AccordionItem>
-              ))}
+          {hammenuItems.map((item, index) => (
+            <div className=" px-5">
+              <div onClick={(e)=>handleActiveDropDown(item.expand,e)} className="flex   justify-between items-center py-4 px-5 relative">
+                <span>{item.main}</span>
+                {item.children && <AiFillCaretDown />}
+              </div>
+              <div className=" w-full   px-5">
+                {IsExpand[item.expand] && DropDown===item.expand && item.children &&
+                  item?.children?.map((subitem) => (
+                    <>
+                      <div onClick={()=>handleActiveChildDropDown(subitem.expand)}  className="flex justify-between items-center mt-2  ">
+                        <span className="py-4">{subitem.childmenu}</span>
+                        {subitem.subchildren && <AiFillCaretDown />}
+                      </div>
+                      { IsExpandChild[subitem.expand] && DropDownChild===subitem.expand && subitem?.subchildren?.map((subChildren) => (
+                        <div className="flex justify-between items-center  mt-2 ">
+                          <span className="py-4">{subChildren.subchild}</span>
+                        </div>
+                      ))}
+                    </>
+                  ))}
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       ) : null}
     </div>
@@ -88,3 +88,19 @@ const HamburgerMenu = () => {
 
 export default HamburgerMenu;
 
+{
+  /* <div className="relative px-5">
+          <div className="flex justify-between items-center py-4 px-5 relative">
+            <span>asd</span>
+            <AiFillCaretDown />
+            <div className="absolute top-10 w-full flex justify-between items-center py-4 px-5">
+              <span>asda1</span>
+              <AiFillCaretDown />
+              <div className="absolute top-10 w-full flex justify-between items-center py-4 px-10">
+              <span>asda1</span>
+              <AiFillCaretDown />
+            </div>
+            </div>
+          </div>
+          </div> */
+}

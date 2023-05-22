6f82ebffdd04b7c3019c8035596394dbccfffc99
akash-endlos <akash.endlos@gmail.com>
2023-05-22 10:20:49 +0530

    updated

--- src/components/home-page-components/HomeBlogImages/HomeBlogImages.jsx
+++ src/components/home-page-components/HomeBlogImages/HomeBlogImages.jsx
@@ -20,7 +20,7 @@ const HomeBlogImages = () => {
                     <div className="card cursor-pointer">
                     <img src="/assets/home_vr_1.jpg" className="w-full h-auto " alt="example image" />
                         <div className="absolute inset-0 bg-black bg-opacity-50 text-white flex items-end justify-start opacity-0 transition duration-300 ease-in-out hover:opacity-100">
-                            <span className="xl:text-4xl md:text-sm font-bold absolute bottom-16 left-10">Make Our World Beautiful With Virtual Reality</span>
+                            <span className="xl:text-4xl md:text-sm font-bold absolute bottom-16 left-10">Artificial Intelligence in Gaming: Enhancing Immersion and Gameplay.</span>
                         </div>
                     </div>
                 </div>

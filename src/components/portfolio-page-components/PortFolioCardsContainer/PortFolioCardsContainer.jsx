7346759de9cab101fb062d3dc12a48d55f9f353f
akash-endlos <akash.endlos@gmail.com>
2023-05-26 15:18:03 +0530

    first-commit

--- src/components/portfolio-page-components/PortFolioCardsContainer/PortFolioCardsContainer.jsx
+++ src/components/portfolio-page-components/PortFolioCardsContainer/PortFolioCardsContainer.jsx
@@ -7,22 +7,18 @@ const PortFolioCardsContainer = () => {
                 <div className=" py-4  " >
                     <img className="w-full h-48 object-cover mb-2" src="/assets/inner-space.jpg" alt="Card Image" />
                     <h2 className="text-xl font-semibold mb-2 text-[#b6b6b6]">Visualization</h2>
-                    {/* <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
                 </div>
                 <div className=" py-4 ">
                     <img className="w-full h-48 object-cover mb-2" src="/assets/LED_live_action.jpg" alt="Card Image" />
                     <h2 className="text-xl font-semibold mb-2 text-[#b6b6b6]">LED Live Action</h2>
-                    {/* <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
                 </div>
                 <div className=" py-4 ">
                     <img className="w-full h-48 object-cover mb-2" src="/assets/Motion_Capture.jpg" alt="Card Image" />
                     <h2 className="text-xl font-semibold mb-2 text-[#b6b6b6]">Motion Capture</h2>
-                    {/* <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
                 </div>
                 <div className=" py-4 ">
                     <img className="w-full h-48 object-cover mb-2" src="/assets/Hybrid_camera.jpg" alt="Card Image" />
                     <h2 className="text-xl font-semibold mb-2 text-[#b6b6b6]">Hybrid Camera</h2>
-                    {/* <p className="text-[#b6b6b6]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
                 </div>
             </div>
         </div>

9901e8494bd307169ad4f81a66507b80f64996b3
akash-endlos <akash.endlos@gmail.com>
2023-05-30 15:13:45 +0530

    updated

--- src/components/about-page-components/ParallaxImageContainer/ParallaxImageContainer.jsx
+++ src/components/about-page-components/ParallaxImageContainer/ParallaxImageContainer.jsx
@@ -13,7 +13,7 @@ const ParallaxImageContainer = () => {
             }} className="absolute top-0 left-0 w-full h-full bg-fixed bg-center bg-gradient-to-b from-gray-900 to-gray-600"></div>
             <div className="absolute inset-0 flex items-center justify-center">
                 <div className='flex flex-col items-center gap-2'>
-                <p className='text-xl text-white'>FILMING CREW</p>
+                {/* <p className='text-xl text-white'>FILMING CREW</p> */}
                 <h1 className="text-white text-3xl font-bold font-serif text-center">MEET THE TEAM</h1>
                 </div>
             </div>

0eb1cbdbff8d37bd0dff7a0a127c8b8ca5523a1f
akash-endlos <akash.endlos@gmail.com>
2023-05-30 11:57:47 +0530

    updated

--- src/components/about-page-components/CreativeTeam/CreativeTeam.jsx
+++ src/components/about-page-components/CreativeTeam/CreativeTeam.jsx
@@ -7,34 +7,29 @@ const CreativeTeam = () => {
                 {/* <p className='text-center font-bold text-4xl text-white py-10'>CREATIVE TEAM</p> */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pb-10 text-center">
                     <div className=" p-4 ">
-                        <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
-                        <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">NATHAN STANLEY</h2>
-                        <p className="text-[#b6b6b6]">Lorem ipsum</p>
+                        <img className="w-full h-48 object-cover mb-2" src="/assets/humanicon-01.png" alt="Card Image" />
+                        <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">YASH PATEL</h2>
+                        <p className="text-[#b6b6b6]"> CEO and director of Endlos group</p>
                     </div>
                     <div className=" p-4 ">
-                        <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
-                        <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">HOWARDLEY CLARK</h2>
-                        <p className="text-[#b6b6b6]">Lorem ipsum</p>
+                        <img className="w-full h-48 object-cover mb-2" src="/assets/humanicon-01.png" alt="Card Image" />
+                        <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">KHILAN PATEL</h2>
+                        <p className="text-[#b6b6b6]"> Founder and director of  Endlos group</p>
                     </div>
                     <div className=" p-4 ">
-                        <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
+                        <img className="w-full h-48 object-cover mb-2" src="/assets/humanicon-01.png" alt="Card Image" />
                         <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">ROHIT NAIR</h2>
-                        <p className="text-[#b6b6b6]">Lorem ipsum</p>
+                        <p className="text-[#b6b6b6]"> Unreal Engine Lead</p>
                     </div>
                     <div className=" p-4 ">
-                        <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
-                        <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">TONY STARK</h2>
-                        <p className="text-[#b6b6b6]">Lorem ipsum</p>
+                        <img className="w-full h-48 object-cover mb-2" src="/assets/humanicon-01.png" alt="Card Image" />
+                        <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">NIKESH MORE</h2>
+                        <p className="text-[#b6b6b6]">Unreal Engine Artist</p>
                     </div>
                     <div className=" p-4 ">
-                        <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
-                        <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">JARVIS</h2>
-                        <p className="text-[#b6b6b6]">Lorem ipsum</p>
-                    </div>
-                    <div className=" p-4 ">
-                        <img className="w-full h-48 object-cover mb-2" src="https://source.unsplash.com/random/800x600" alt="Card Image" />
-                        <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">SOMEONE I DONT KNOW</h2>
-                        <p className="text-[#b6b6b6]">Lorem ipsum</p>
+                        <img className="w-full h-48 object-cover mb-2" src="/assets/humanicon-01.png" alt="Card Image" />
+                        <h2 className="text-lg font-semibold mb-2 text-[#b6b6b6]">PAVAN BORSE</h2>
+                        <p className="text-[#b6b6b6]">Unreal Engine Artist</p>
                     </div>
                 </div>
             </div>

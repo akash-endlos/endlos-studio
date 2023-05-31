6fcef76c153eab9fed438bb2aeb07ad05b0f039a
akash-endlos <akash.endlos@gmail.com>
2023-05-31 16:10:19 +0530

    updated

--- src/components/blog-page-components/AlternateImageContainer/AlternateImageContainer.jsx
+++ src/components/blog-page-components/AlternateImageContainer/AlternateImageContainer.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 
 const AlternateImageContainer = ({filteredData}) => {
-    const blogData = useSelector((state)=>state.allblogs.blog)
     const LeftContainer = ({id,name,src}) => (
         <>
                     <div className="max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -23,41 +22,13 @@ const AlternateImageContainer = ({filteredData}) => {
                     </div>
         </>
     )
-    const RightContainer = ({id,name,src}) => (
-        <>
-
-                        <div className="max-w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <a href="#">
-                                <img className="rounded-t-lg" src={src} alt="img_2" />
-                            </a>
-                            <div className="p-5">
-                                <a href="#">
-                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
-                                </a>
-
-                                <Link href={`blog/${id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    Read more
-                                    <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                </Link>
-                            </div>
-                        </div>
-        </>
-    )
     return (
         <div className='bg-[#111111] text-white'>
             <div className='max-w-screen-2xl grid xl:grid-cols-2 md:grid-cols-1 gap-5 mx-auto px-10'>
-               {filteredData && filteredData.map((item,key)=>{
-                if(item.value==='left')
-                {return(
-                    <LeftContainer id={item?.id} name={item.name} src={item.src} />
-                   
-                )}
-                else{
-                    return(
-                        <RightContainer id={item?.id} name={item.name} src={item.src} />  
-                    )
-                }
-               })}
+               {filteredData && filteredData.map((item,key)=>(
+                   <>
+                   <LeftContainer id={item?.id} name={item.name} src={item.src} />
+                   </> ))}
             </div>
 
         </div>

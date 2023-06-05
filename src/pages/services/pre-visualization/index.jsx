d55ec1318f28b1f869f52d9adcce275b73c5bbc7
akash-endlos <akash.endlos@gmail.com>
2023-06-05 18:36:17 +0530

    updated

--- src/pages/services/pre-visualization/index.jsx
+++ src/pages/services/pre-visualization/index.jsx
@@ -21,7 +21,7 @@ const index = () => {
 <div class="z-30 relative items-center justify-center w-full h-full ">
     <div class="inset-0 h-[600px] bg-cover bg-center" 
         style={{
-          backgroundImage: `url("https://wallpapercave.com/wp/wp6689710.jpg")`,
+          backgroundImage: `url("/assets/contact_us.jpg")`,
         }}
     >
     </div>
@@ -29,27 +29,27 @@ const index = () => {
     <div class="absolute inset-0  z-30  flex flex-col items-center justify-center">
         <div class="shadow-2xl rounded-lg w-4/5 h-96 bg-cover bg-center"
              style={{
-              backgroundImage: `url("https://wallpapercave.com/wp/wp6689710.jpg")`,
+              backgroundImage: `url("/assets/contact_us.jpg")`,
             }}>
 
             <div class="grid grid-cols-12 gap-1">
                 <div class="relative my-6 px-8 col-span-12 sm:col-span-12 md:col-span-7 lg:col-span-7 xxl:col-span-7">
-                    <div class="border-l-4 border-gray-400 py-20 px-5 mx-2 absolute left-0">
+                    <div class="border-l-4 border-gray-200 py-20 px-5 mx-2 absolute left-0">
                         <p class="italic text-white text-xl md:text-4xl lg:text-6xl uppercase text-center  font-semibold ">
                         Previsualization
                         </p>
                     </div>
-                    <div class="text-gray-400 font-semibold text-xl mb-4">Previsualization</div>
-                    <div class="absolute border-gray-400 border-t-4 bottom-0 py-1 px-4 w-4/5"></div>
+                    <div class="text-white font-semibold text-xl mb-4">Previsualization</div>
+                    <div class="absolute border-gray-200 border-t-4 bottom-0 py-1 px-4 w-4/5"></div>
                 </div>
                 <div class="col-span-12 sm:col-span-12 md:col-span-5 lg:col-span-5 xxl:col-span-5">
-                    <div class="relative bg-pink-900 h-full md:h-96 w-full bg-opacity-50 rounded-tr-lg rounded-br-lg">
+                    <div class="relative bg-green-900 h-full md:h-96 w-full bg-opacity-50 rounded-tr-lg rounded-br-lg">
                         <div class="p-8">
-                            <p class="text-white text-xs md:text-sm lg:text-xl mb-4">
+                            <p class="text-gray-100 text-xs md:text-sm lg:text-xl mb-4">
                             Previsualization, often referred to as previs, is the process of creating a preliminary visual representation of a film, animation, or any visual media project before it goes into production.
                             </p>
                             <div class="bottom-0 absolute p-2 right-0">
-                                <button class="opacity-75  hover:bg-pink-900 hover:text-white text-sm font-bold py-2 px-4 rounded inline-flex items-center">
+                                <button class="opacity-75  hover:bg-gray-900 hover:text-white text-sm font-bold py-2 px-4 rounded inline-flex items-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                     </svg>

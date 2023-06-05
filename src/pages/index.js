67a2c7c772f50fdd76ddbd6223e07ccdd7f37aa8
akash-endlos <akash.endlos@gmail.com>
2023-06-05 12:11:44 +0530

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,45 @@
 import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Head from "next/head";
+import Navbar from "@/components/navbar/Navbar";
 import HomeCarousel from "@/components/home-page-components/HomeCarousel/HomeCarousel";
 import HomeImageContainer from "@/components/home-page-components/HomeImageContainer/HomeImageContainer";
 import HomeServices from "@/components/home-page-components/HomseServices/HomeServices";
 import HomeVideoCarousel from "@/components/home-page-components/HomeVideoCarousel/HomeVideoCarousel";
 import HomeBlogImages from "@/components/home-page-components/HomeBlogImages/HomeBlogImages";
 import HomeWriteUsContainer from "@/components/home-page-components/HomeWriteUsContainer/HomeWriteUsContainer";
-import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 import HomeProductionHouse from "@/components/home-page-components/HomeProductionHouse/HomeProductionHouse";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [metaTags, setMetaTags] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/v1/head/get?webPageUrl=http://localhost:3001")
+      .then((res) => {
+        const headTag = res.data.data.WebDetails.headTag;
+        setMetaTags(headTag);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch meta tags:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (metaTags) {
+      const headTagElement = document.createElement("div");
+      headTagElement.innerHTML = metaTags;
+      const headElement = document.querySelector("head");
+      headElement.appendChild(headTagElement);
+    }
+  }, [metaTags]);
+
   return (
     <>
-      <Head>
-        <title>Endlos Studio | Unreal Engine Possibilities</title>
-        <meta name="description" content="Endlos Studio offers virtual production, architectural visualization & game development expertise. Experience the power of Unreal Engine 5.2 with us." />
-        <meta name="keywords" content="unreal engine, Virtual Production, Architectural Visualization, Game Development, unreal engine 5.2, Endlos Studio" />
-        <meta property="og:title" content="Unreal Engine 5.2 is now available! | Endlos Studio" />
-        <meta property="og:description" content="Endlos Studio offers virtual production, architectural visualization & game development expertise. Experience the power of Unreal Engine 5.2 with us." />
-        <meta property="og:site_name" content="Endlos Studio " />
-        <meta property="og:type" content="article" />
-        <meta property="og:image" content="https://endlos.studio/assets/logo.png" />
-        <meta property="og:url" content="https://endlos.studio/ " />
-        <meta name="twitter:title" content="Unreal Engine 5.2 is now available! | Endlos Studio" />
-        <meta name="twitter:description" content="Endlos Studio offers virtual production, architectural visualization & game development expertise. Experience the power of Unreal Engine 5.2 with us." />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://endlos.studio/" />
-        <meta name="twitter:image" content="http://localhost:3000/assets/logo.png" />
-        <meta name="robots" content="index, follow" />
-        <link rel="canonical" href="https://endlos.studio/" />
-      </Head>
       <Navbar />
       <HomeCarousel />
       <HomeImageContainer />

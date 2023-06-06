0fad720ad18926430a1d73a0827b473c812c6ba9
akash-endlos <akash.endlos@gmail.com>
2023-06-06 10:45:31 +0530

    updated

--- src/components/home-page-components/HomeCarousel/HomeCarousel.jsx
+++ src/components/home-page-components/HomeCarousel/HomeCarousel.jsx
@@ -34,12 +34,16 @@ const HomeCarousel = () => {
       >
         {CarouselSection.map((item,index) => (
          <SwiperSlide key={index}>
-         <div className="relative">
-           <video src={item.src} autoPlay loop muted  className={styles.videocontainer} />
-           <div className="absolute top-10 left-0 w-full h-full flex items-center justify-center px-5 py-16">
-             <p className="text-white xl:text-5xl md:text-xl shadow-2xl rounded-lg  font-bold">Shaping Digital Realities for Endless Possibilities</p>
-           </div>
-         </div>
+       <div className="relative">
+  <video src={item.src} autoPlay loop muted className={`${styles.videocontainer}  `} />
+  <div className="absolute top-0 left-0 w-full h-full flex flex-col items-start gap-4 justify-center px-5 bg-opacity-60 bg-black ">
+    <p className="text-white xl:text-5xl md:text-xl max-w-screen-md px-5 font-extrabold">Shaping Digital Realities for</p>
+    <span className="text-white xl:text-5xl md:text-xl max-w-screen-md px-5 font-extrabold">Endless Possibilities</span>
+    <p className="text-white xl:text-2xl md:text-xl max-w-screen-md px-5 font-extrabold leading-10">Welcome to Endlos Studio, a leading creative powerhouse specializing in harnessing the full potential of Unreal Engine.</p>
+    <div className="px-5"><button className="px-5 py-3 rounded-lg font-semibold border border-transparent bg-[#111111] text-white hover:bg-white hover:text-black">Contact Us</button></div>
+  </div>
+</div>
+
        </SwiperSlide>
        
         ))}

1e7d20a8489035506e61d5654bf7e3571bb302fd
akash-endlos <akash.endlos@gmail.com>
2023-06-07 15:08:22 +0530

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,24 +27,8 @@ export default function Home() {
       dispatch(getMetatags("SET_META_TAGS", hostWithPath));
     }
   }, [dispatch, path]);
-  // const path = useRouter().pathname;
-  // const metaTags = useSelector(state => state.allMetatags.payload);
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   if (window) {
-  //     const host = window.location.host
-  //     const hostWithPath = `${host}${path}`
-  //     dispatch(getMetatags('SET_META_TAGS', hostWithPath));
-  //   }
-  // }, [dispatch, path]);
   return (
     <>
-      {/* <Head>
-        {metaTags && (
-          <meta dangerouslySetInnerHTML={{ __html: metaTags }} />
-        )}
-      </Head> */}
       <HeadTag/>
       <Navbar />
       <HomeCarousel />

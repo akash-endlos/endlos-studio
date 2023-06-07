f556633b00efa97a33b1ad6110b94ef46f8e982e
akash-endlos <akash.endlos@gmail.com>
2023-06-07 10:27:05 +0530

    updated

--- src/pages/services/virtual-production/index.jsx
+++ src/pages/services/virtual-production/index.jsx
@@ -74,28 +74,22 @@ const index = () => {
 
 
         <div className='max-w-screen-2xl mx-auto'>
-          <p className='text-white font-semibold py-10'>HOW WE DO IT</p>
-          <div className="grid grid-cols-12  py-10">
-            <div className="col-span-4">
-              <div className='flex flex-col items-start justify-center'>
-                <h2 className="text-5xl text-white max-w-screen-sm font-bold mb-4">Your Vision.
-                </h2>
-                <span className="text-5xl text-white max-w-screen-sm font-bold mb-4">Our Studio Network.</span>
+          <p className='text-white font-semibold py-10 flex justify-center xl:justify-start '>HOW WE DO IT</p>
+          <div className="max-w-screen-2xl mx-auto px-5">
+          <div className="grid xl:grid-cols-12 lg:grid-flow-row py-10">
+            <div className="col-span-12 lg:col-span-4">
+              <div className="flex flex-col items-start justify-center">
+                <h2 className="text-4xl  text-white max-w-sm font-bold mb-4">Your Vision Our Studio Network</h2>
               </div>
             </div>
-            <div className="col-span-8">
-              <div className="flex flex-col justify-start text-white h-full text-xl leading-10">
+            <div className="col-span-12 lg:col-span-8">
+              <div className="flex flex-col justify-start text-white h-full text-xl  leading-8">
                 <p className="mb-4 text-justify">With the Vū Network™, we combine the power of creativity and technology to create an environment for collaboration, creative control and freedom. Blurring the lines between what's physical and what's digital, we can take you anywhere you want to go - all from a single location.</p>
               </div>
             </div>
-
-
           </div>
         </div>
-
-
-        <h2 className='font-bold text-3xl text-center text-white py-10'>HOW CAN THIS SERVICE HELP YOU</h2>
-        <p className='py-10 text-white max-w-screen-2xl mx-auto text-center'>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet, a cursus ipsum ante quis turpis urna non tincidunt mattis, tortor neque adipiscing.</p>
+        </div>
         <div className='max-w-screen-2xl mx-auto py-1'>
           <div className="grid grid-cols-12 gap-10 py-10">
             <div className="col-span-6">
@@ -111,21 +105,23 @@ const index = () => {
               <img src="/assets/blog_inside_vr_2.jpg" alt="Image" className="w-full h-[500px] rounded-2xl" />
             </div>
           </div>
-          <p className='text-white font-semibold py-10'>HOW WE DO IT</p>
-          <div className="grid grid-cols-12  py-10">
-            <div className="col-span-4">
-              <div className='flex flex-col items-start justify-center'>
-                <h2 className="text-5xl text-white max-w-screen-sm font-bold mb-4">Your Vision.
-                </h2>
-                <span className="text-5xl text-white max-w-screen-sm font-bold mb-4">Our Studio Network.</span>
+          <div className='max-w-screen-2xl mx-auto'>
+          <p className='text-white font-semibold py-10 flex justify-center xl:justify-start '>HOW WE DO IT</p>
+          <div className="max-w-screen-2xl mx-auto px-5">
+          <div className="grid xl:grid-cols-12 lg:grid-flow-row py-10">
+            <div className="col-span-12 lg:col-span-4">
+              <div className="flex flex-col items-start justify-center">
+                <h2 className="text-4xl  text-white max-w-sm font-bold mb-4">Your Vision Our Studio Network</h2>
               </div>
             </div>
-            <div className="col-span-8">
-              <div className="flex flex-col justify-start text-white h-full text-xl leading-10">
+            <div className="col-span-12 lg:col-span-8">
+              <div className="flex flex-col justify-start text-white h-full text-xl  leading-8">
                 <p className="mb-4 text-justify">With the Vū Network™, we combine the power of creativity and technology to create an environment for collaboration, creative control and freedom. Blurring the lines between what's physical and what's digital, we can take you anywhere you want to go - all from a single location.</p>
               </div>
             </div>
           </div>
+        </div>
+        </div>
           <div className="grid grid-cols-12 gap-10 py-10">
             <div className="col-span-6">
               <img src="/assets/blog_inside_vr_2.jpg" alt="Image" className="w-full h-[500px] rounded-2xl" />
@@ -155,21 +151,23 @@ const index = () => {
               <img src="/assets/blog_inside_vr_2.jpg" alt="Image" className="w-full h-[500px] rounded-2xl" />
             </div>
           </div>
-          <p className='text-white font-semibold py-10'>HOW WE DO IT</p>
-          <div className="grid grid-cols-12  py-10">
-            <div className="col-span-4">
-              <div className='flex flex-col items-start justify-center'>
-                <h2 className="text-5xl text-white max-w-screen-sm font-bold mb-4">Your Vision.
-                </h2>
-                <span className="text-5xl text-white max-w-screen-sm font-bold mb-4">Our Studio Network.</span>
+          <div className='max-w-screen-2xl mx-auto'>
+          <p className='text-white font-semibold py-10 flex justify-center xl:justify-start '>HOW WE DO IT</p>
+          <div className="max-w-screen-2xl mx-auto px-5">
+          <div className="grid xl:grid-cols-12 lg:grid-flow-row py-10">
+            <div className="col-span-12 lg:col-span-4">
+              <div className="flex flex-col items-start justify-center">
+                <h2 className="text-4xl  text-white max-w-sm font-bold mb-4">Your Vision Our Studio Network</h2>
               </div>
             </div>
-            <div className="col-span-8">
-              <div className="flex flex-col justify-start text-white h-full text-xl leading-10">
+            <div className="col-span-12 lg:col-span-8">
+              <div className="flex flex-col justify-start text-white h-full text-xl  leading-8">
                 <p className="mb-4 text-justify">With the Vū Network™, we combine the power of creativity and technology to create an environment for collaboration, creative control and freedom. Blurring the lines between what's physical and what's digital, we can take you anywhere you want to go - all from a single location.</p>
               </div>
             </div>
           </div>
+        </div>
+        </div>
           <div className="grid grid-cols-12 gap-10 py-10">
             <div className="col-span-6">
               <img src="/assets/blog_inside_vr_2.jpg" alt="Image" className="w-full h-[500px] rounded-2xl" />

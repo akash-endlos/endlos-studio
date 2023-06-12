fc5d9ea8b7b84a3f5b07b20627aad83101ff3d5d
akash-endlos <akash.endlos@gmail.com>
2023-06-12 19:03:30 +0530

    updated

--- src/pages/services/archviz/walkthrough/index.jsx
+++ src/pages/services/archviz/walkthrough/index.jsx
@@ -38,12 +38,12 @@ const index = () => {
           <div className="grid xl:grid-cols-12 lg:grid-flow-row py-10">
             <div className="col-span-12 lg:col-span-4">
               <div className="flex flex-col items-start justify-center">
-                <h2 className="text-4xl  text-white max-w-sm font-bold mb-4">Your Vision Our Studio Network</h2>
+                <h2 className="text-4xl  text-white max-w-sm font-bold mb-4">Why Choose Endlos Studio</h2>
               </div>
             </div>
             <div className="col-span-12 lg:col-span-8">
               <div className="flex flex-col justify-start text-white h-full text-xl  leading-8">
-                <p className="mb-4 text-justify">With the Vū Network™, we combine the power of creativity and technology to create an environment for collaboration, creative control and freedom. Blurring the lines between what's physical and what's digital, we can take you anywhere you want to go - all from a single location.</p>
+                <p className="mb-4 text-justify">Engage and captivate our audience with interactive experiences that push the boundaries of technology and creativity. Our interactive walkthrough services cover various applications, including virtual reality (VR) and augmented reality (AR) experiences, custom installations for events and exhibitions, and multi-platform development for web and mobile. With our expertise in the user interface (UI) and user experience (UX) design, we create intuitive and enjoyable interactive experiences that leave a lasting impression.</p>
               </div>
             </div>
           </div>
@@ -95,12 +95,12 @@ const index = () => {
             <div className="grid xl:grid-cols-12 lg:grid-flow-row py-10">
               <div className="col-span-12 lg:col-span-4">
                 <div className="flex flex-col items-start justify-center">
-                  <h2 className="text-4xl  text-white max-w-sm font-bold mb-4">Your Vision Our Studio Network</h2>
+                  <h2 className="text-4xl  text-white max-w-sm font-bold mb-4">Walkthrough Services</h2>
                 </div>
               </div>
               <div className="col-span-12 lg:col-span-8">
                 <div className="flex flex-col justify-start text-white h-full text-xl  leading-8">
-                  <p className="mb-4 text-justify">With the Vū Network™, we combine the power of creativity and technology to create an environment for collaboration, creative control and freedom. Blurring the lines between what's physical and what's digital, we can take you anywhere you want to go - all from a single location.</p>
+                  <p className="mb-4 text-justify">At Endlos Studio, we offer walkthrough services in architectural visualization that allow you to experience and explore architectural spaces in a virtual environment. Our walkthroughs comprehensively understand your designs, helping you showcase and communicate your architectural vision with precision and impact.</p>
                 </div>
               </div>
             </div>
@@ -110,18 +110,20 @@ const index = () => {
           <div className="grid grid-cols-12  py-10">
             <div className="xl:col-span-6 col-span-12">
               <div className="flex flex-col justify-start items-center xl:items-start text-white h-full">
-                <h2 className="text-5xl max-w-screen-sm font-bold mb-4">Environment Design and Production Management</h2>
-                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Unreal Environment Design</h2>
-                <p className="mb-4 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, dolorem quod ex assumenda dolorum voluptas beatae, officiis alias, tempore voluptatibus nam. Eligendi similique illum rerum quos voluptatibus alias dolore asperiores. Blanditiis ducimus ea, magni, praesentium eligendi a tempora distinctio aspernatur laboriosam illo ipsum laudantium voluptatem at obcaecati accusantium eos maxime. </p>
-                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Unreal Environment Design</h2>
-                <p className="mb-4 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, dolorem quod ex assumenda dolorum voluptas beatae, officiis alias, tempore voluptatibus nam. Eligendi similique illum rerum quos voluptatibus alias dolore asperiores. </p>
+                {/* <h2 className="text-5xl max-w-screen-sm font-bold mb-4">Environment Design and Production Management</h2> */}
+                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Virtual Exploration</h2>
+                <p className="mb-4 text-justify">Our walkthroughs enable you to navigate architectural spaces virtually, providing a realistic and immersive experience. With advanced 3D rendering techniques, we create detailed environments that accurately represent your designs, allowing clients and stakeholders to engage and interact with the spaces. </p>
+                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Attention to Detail</h2>
+                <p className="mb-4 text-justify">We pay meticulous attention to detail, ensuring that every aspect of your architectural design is faithfully represented in the walkthrough. From textures and materials to lighting and spatial relationships, we strive for accuracy and realism, creating an authentic representation of the space.</p>
+                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Interactive Elements</h2>
+                <p className="mb-4 text-justify">Our walkthroughs can incorporate interactive elements, such as clickable hotspots, informative pop-ups, and annotations, to provide additional information about specific areas or features of the design. This interactivity enhances engagement and facilitates a deeper understanding of the space.</p>
               </div>
             </div>
             <div className="xl:col-span-6 col-span-12 xl:pl-5">
               <img src="/assets/Walktrough_1.jpg" alt="Image" className="w-full h-auto rounded-2xl" />
             </div>
           </div>
-          <div className='max-w-screen-2xl mx-auto'>
+          {/* <div className='max-w-screen-2xl mx-auto'>
             <p className='text-white font-semibold py-10 flex justify-center xl:justify-start '>HOW WE DO IT</p>
             <div className="max-w-screen-2xl mx-auto px-5">
               <div className="grid xl:grid-cols-12 lg:grid-flow-row py-10">
@@ -137,23 +139,24 @@ const index = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
           <div className="grid grid-cols-12 py-10">
             <div className="xl:col-span-6 col-span-12">
               <img src="/assets/Walktrough_2.jpg" alt="Image" className="w-full h-auto rounded-2xl" />
             </div>
             <div className="xl:col-span-6 col-span-12 xl:pl-5">
               <div className="flex flex-col justify-end text-white h-full">
-                <h2 className="text-5xl max-w-screen-sm font-bold mb-4">Environment Design and Production Management</h2>
-                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Unreal Environment Design</h2>
-                <p className="mb-4 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, dolorem quod ex assumenda dolorum voluptas beatae, officiis alias, tempore voluptatibus nam. Eligendi similique illum rerum quos voluptatibus alias dolore asperiores. Blanditiis ducimus ea, magni, praesentium eligendi a tempora distinctio aspernatur laboriosam illo ipsum laudantium voluptatem at obcaecati accusantium eos maxime. </p>
-                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Unreal Environment Design</h2>
-                <p className="mb-4 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, dolorem quod ex assumenda dolorum voluptas beatae, officiis alias, tempore voluptatibus nam. Eligendi similique illum rerum quos voluptatibus alias dolore asperiores. </p>
+                {/* <h2 className="text-5xl max-w-screen-sm font-bold mb-4">Environment Design and Production Management</h2> */}
+                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Realistic Lighting and Atmosphere</h2>
+                <p className="mb-4 text-justify">We create realistic lighting scenarios that replicate natural daylight or artificial lighting sources, enhancing the visual appeal of the walkthrough. The interplay of light and shadow and the manipulation of atmosphere and mood add depth and realism to the virtual space. </p>
+                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Customization and Flexibility</h2>
+                <p className="mb-4 text-justify">We understand that every architectural project is unique, and we tailor our walkthroughs to meet your specific requirements. Whether showcasing different design options, highlighting specific architectural features, or incorporating branding elements, we offer customization and flexibility to ensure that the walkthrough aligns perfectly with your vision. </p>
+                <p className="mb-4 text-justify">Our walkthrough services in architectural visualization provide a powerful tool for architects, interior designers, and real estate developers to effectively communicate their designs to clients, investors, and the general public. With attention to detail, realism, and interactive features, we bring your architectural vision to life and create a compelling visual experience.</p>
               </div>
             </div>
 
           </div>
-          <div className="grid grid-cols-12  py-10">
+          {/* <div className="grid grid-cols-12  py-10">
             <div className="xl:col-span-6 col-span-12">
               <div className="flex flex-col justify-start items-center xl:items-start text-white h-full">
                 <h2 className="text-5xl max-w-screen-sm font-bold mb-4">Environment Design and Production Management</h2>
@@ -166,19 +169,20 @@ const index = () => {
             <div className="xl:col-span-6 col-span-12 xl:pl-5">
               <img src="/assets/Walktrough_3.jpg" alt="Image" className="w-full h-auto rounded-2xl" />
             </div>
-          </div>
+          </div> */}
           <div className='max-w-screen-2xl mx-auto'>
             <p className='text-white font-semibold py-10 flex justify-center xl:justify-start '>HOW WE DO IT</p>
             <div className="max-w-screen-2xl mx-auto px-5">
               <div className="grid xl:grid-cols-12 lg:grid-flow-row py-10">
                 <div className="col-span-12 lg:col-span-4">
                   <div className="flex flex-col items-start justify-center">
-                    <h2 className="text-4xl  text-white max-w-sm font-bold mb-4">Your Vision Our Studio Network</h2>
+                    <h2 className="text-4xl  text-white max-w-sm font-bold mb-4">Architectural Visualization (Walkthrough)</h2>
                   </div>
                 </div>
                 <div className="col-span-12 lg:col-span-8">
                   <div className="flex flex-col justify-start text-white h-full text-xl  leading-8">
-                    <p className="mb-4 text-justify">With the Vū Network™, we combine the power of creativity and technology to create an environment for collaboration, creative control and freedom. Blurring the lines between what's physical and what's digital, we can take you anywhere you want to go - all from a single location.</p>
+                    <p className="mb-4 text-justify">Architectural visualization walkthroughs are powerful tools used to showcase and communicate architectural designs in a dynamic and immersive way. Through advanced 3D rendering techniques and animation, walkthroughs provide a virtual tour of a building or space, allowing viewers to experience and explore the design from various angles and perspectives.</p>
+                    <p className="mb-4 text-justify">Our studio specializes in creating high-quality architectural visualization walkthroughs that bring architectural designs to life. Our portfolio demonstrates our expertise in this field, highlighting the projects we have successfully delivered.</p>
                   </div>
                 </div>
               </div>
@@ -190,20 +194,38 @@ const index = () => {
             </div>
             <div className="xl:col-span-6 col-span-12 xl:pl-5">
               <div className="flex flex-col justify-end text-white h-full">
-                <h2 className="text-5xl max-w-screen-sm font-bold mb-4">Environment Design and Production Management</h2>
-                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Unreal Environment Design</h2>
-                <p className="mb-4 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, dolorem quod ex assumenda dolorum voluptas beatae, officiis alias, tempore voluptatibus nam. Eligendi similique illum rerum quos voluptatibus alias dolore asperiores. Blanditiis ducimus ea, magni, praesentium eligendi a tempora distinctio aspernatur laboriosam illo ipsum laudantium voluptatem at obcaecati accusantium eos maxime. </p>
-                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Unreal Environment Design</h2>
-                <p className="mb-4 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, dolorem quod ex assumenda dolorum voluptas beatae, officiis alias, tempore voluptatibus nam. Eligendi similique illum rerum quos voluptatibus alias dolore asperiores. </p>
+                {/* <h2 className="text-5xl max-w-screen-sm font-bold mb-4">Environment Design and Production Management</h2> */}
+                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Realistic Visualization</h2>
+                <p className="mb-4 text-justify">We use state-of-the-art rendering techniques to create realistic and visually stunning walkthroughs. Our attention to detail in texturing, lighting, and materiality ensures that the virtual representation closely matches the intended real-world design.</p>
+                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Interior and Exterior Exploration</h2>
+                <p className="mb-4 text-justify">Our walkthroughs enable viewers to explore the architectural design's interior and exterior. Users can virtually move through rooms, navigate hallways, and experience the flow of spaces. They can also observe the building's exterior from different angles, understanding its form and integration with the surrounding environment.</p>
+                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Mood and Atmosphere</h2>
+                <p className="mb-4 text-justify">We understand the importance of capturing a design's intended mood and atmosphere. Our walkthroughs incorporate lighting effects, weather conditions, and material qualities to evoke the desired emotions and create an immersive experience.</p>
               </div>
             </div>
 
+          </div>
+          <div className="grid grid-cols-12  py-10">
+            <div className="xl:col-span-6 col-span-12">
+              <div className="flex flex-col justify-start items-center xl:items-start text-white h-full">
+                {/* <h2 className="text-5xl max-w-screen-sm font-bold mb-4">Environment Design and Production Management</h2> */}
+                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Attention to Detail</h2>
+                <p className="mb-4 text-justify">We pay meticulous attention to every aspect of the architectural design. Our walkthroughs showcase the intricate details of textures, furnishings, and architectural features, accurately representing the design's aesthetics and functionality. </p>
+                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Animation and Cinematic Techniques</h2>
+                <p className="mb-4 text-justify">We utilize animation and cinematic techniques to enhance the visual storytelling of the walkthroughs. Smooth camera movements, transitions, and dynamic sequences engage viewers and create an engaging narrative that effectively communicates the design's vision and purpose. </p>
+                <h2 className="text-2xl max-w-screen-sm font-bold mb-4">Collaboration and Customization</h2>
+                <p className="mb-4 text-justify">We work closely with architects, designers, and clients to ensure the walkthrough meets their specific goals and requirements. Our collaborative approach allows us to incorporate feedback and make necessary adjustments, resulting in a tailored and customized visualization experience. </p>
+              </div>
+            </div>
+            <div className="xl:col-span-6 col-span-12 xl:pl-5">
+              <img src="/assets/Walktrough_3.jpg" alt="Image" className="w-full h-auto rounded-2xl" />
+            </div>
           </div>
         </div>
         <div className="max-w-screen-2xl mx-auto px-5 py-5">
           <div className='h-auto flex flex-col xl:flex-row md:flex-row gap-5 justify-between items-center'>
             <div className='flex flex-col justify-center gap-5'>
-              <p className='text-white font-bold xl:text-2xl text-md text-justify max-w-screen-md leading-10'>Get ready to collaborate with passionate individuals who are dedicated to pushing the boundaries of creativity. Our community is a hub for artistic synergy and collective growth. Together, we can achieve greatness!</p>
+              <p className='text-white font-bold xl:text-2xl text-md text-justify max-w-screen-md leading-10'>Experience the power of architectural visualization walkthroughs through our portfolio. Each project showcases our commitment to delivering high-quality, immersive experiences that effectively communicate architectural designs, inspire stakeholders, and support decision-making processes.</p>
               <Link href='/contact-us/write-to-us'>
                 <button className='rounded-lg bg-black text-white py-3 px-3 font-semibold hover:bg-white hover:text-black'>Connect With Us</button>
               </Link>

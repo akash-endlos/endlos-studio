64d31041396d3f16d431b4e41e8dfc4e5a329c9a
akash-endlos <akash.endlos@gmail.com>
2023-06-22 19:02:34 +0530

    updated

--- src/components/HeaadTag/HeadTag.jsx
+++ src/components/HeaadTag/HeadTag.jsx
@@ -1,21 +1,13 @@
 import Head from "next/head";
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 export function HeadTag() {
   const metaTags = useSelector((state) => state.allMetatags.payload);
-  useEffect(() => {
-    if (metaTags) {
-      const headTagElement = document.createElement("div");
-      headTagElement.innerHTML = metaTags;
-      const headElement = document.querySelector("head");
-      headElement.appendChild(headTagElement);
-    }
-  }, [metaTags]);
+
   return (
     <Head>
      <meta name="google-site-verification" content="QFRVJOgjP7BGBX8Caj1lv93tsw_r738eYFR5N3wnhMM" />
-      {/* {metaTags && <meta dangerouslySetInnerHTML={{ __html: metaTags }} />} */}
+      {metaTags && <meta dangerouslySetInnerHTML={{ __html: metaTags }} />}
     </Head>
   );
 }

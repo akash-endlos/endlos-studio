f28a88f2444fd40135b4d8e17e077ee492b466fa
akash-endlos <akash.endlos@gmail.com>
2023-06-23 13:11:21 +0530

    updated

--- src/components/HeaadTag/HeadTag.jsx
+++ src/components/HeaadTag/HeadTag.jsx
@@ -5,9 +5,24 @@ export function HeadTag() {
   const metaTags = useSelector((state) => state.allMetatags.payload);
   return (
     <Head>
-     <meta name="google-site-verification" content="QFRVJOgjP7BGBX8Caj1lv93tsw_r738eYFR5N3wnhMM" />
+      <meta name="google-site-verification" content="QFRVJOgjP7BGBX8Caj1lv93tsw_r738eYFR5N3wnhMM" />
       {/* {metaTags && <meta dangerouslySetInnerHTML={{ __html: metaTags }} />} */}
       <title> {metaTags?.title}</title>
+      <meta name="description" content={metaTags?.description} />
+      <meta name="keywords" content={metaTags?.keywords} />
+      <meta property="og:title" content={metaTags?.ogTitle} />
+      <meta property="og:description" content={metaTags?.ogDescription} />
+      <meta property="og:site_name" content="Endlos Studio " />
+      <meta property="og:type" content="article" />
+      <meta property="og:image" content={metaTags?.ogImageUrl}/>
+      <meta property="og:url" content="https://endlos.studio/ " />
+      <meta name="twitter:title" content={metaTags?.twitterTitle}/>
+      <meta name="twitter:description" content={metaTags?.twitterDescription} />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:url" content="https://endlos.studio/" />
+      <meta name="twitter:image" content={metaTags?.twitterImage} />
+      <meta name="robots" content="index, follow" />
+      <link rel="canonical" href="https://endlos.studio/  " />
     </Head>
   );
 }

9825f59db245b07fc189bb56aab70685558c5af7
akash-endlos <akash.endlos@gmail.com>
2023-07-12 17:06:59 +0530

    updated

--- src/shareBlog/shareBlog.jsx
+++ src/shareBlog/shareBlog.jsx
@@ -7,32 +7,35 @@ import {
   TwitterIcon,
   FacebookShareButton,
   FacebookIcon,
-  InstagramShareButton,
-  InstagramIcon
+  LinkedinShareButton, // Import LinkedinShareButton
+  LinkedinIcon // Import LinkedinIcon
 } from 'react-share';
 
-const BlogShare = ({  title }) => {
+const BlogShare = ({ title }) => {
   const [currentUrl, setCurrentUrl] = useState('');
 
   useEffect(() => {
     setCurrentUrl(window.location.href);
   }, []);
-
   return (
     <>
-      <div className=" justify-between items-center hidden xl:flex gap-2">
-        <small className="pr-5 font-semibold text-lg">Share On :- </small>
+      <div className="justify-between items-center hidden xl:flex gap-2">
+        <small className="pr-5 font-semibold text-lg">Share On: </small>
         <WhatsappShareButton url={currentUrl} title={title}>
           <WhatsappIcon size={32} round={true} />
         </WhatsappShareButton>
 
-        <TwitterShareButton url={currentUrl} title={title}>
+        {/* <TwitterShareButton url={currentUrl} title={title}>
           <TwitterIcon size={32} round={true} />
-        </TwitterShareButton>
+        </TwitterShareButton> */}
 
         <FacebookShareButton url={currentUrl} quote={title}>
           <FacebookIcon size={32} round={true} />
         </FacebookShareButton>
+
+        <LinkedinShareButton url={currentUrl} source={title} title={title} summary={title}>
+          <LinkedinIcon size={32} round={true} />
+        </LinkedinShareButton>
       </div>
     </>
   );
